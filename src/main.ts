602860f0bc5d6d73583a35e7b17b8ea539b80a3b
yike5460 <yike5460@163.com>
2023-08-05 17:46:30 +0800

    fix: issue: (1) Could not find model data at s3:// (2) Property validation failure in EndpointConfigName

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { App, CfnOutput, Stack, StackProps } from 'aws-cdk-lib';
+import { App, CfnOutput, Stack, StackProps, CfnParameter } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 
 import { VpcStack } from './vpc-stack';
@@ -7,6 +7,7 @@ import { OpenSearchStack } from './os-stack';
 import { LLMApiStack } from './api-stack';
 import { DynamoDBStack } from './ddb-stack';
 import { LLMStack } from './llm-stack';
+import { AssetsStack } from './assets-stack';
 
 import * as dotenv from "dotenv";
 dotenv.config();
@@ -14,6 +15,13 @@ dotenv.config();
 export class RootStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps = {}) {
     super(scope, id, props);
+
+    // add cdk input parameters for user to specify s3 bucket store model assets
+    const _S3ModelAssets = new CfnParameter(this, 'S3ModelAssets', {
+      type: 'String',
+      description: 'S3 Bucket for model & code assets',
+      default: 'llm-rag',
+    });
 
     const _VpcStack = new VpcStack(this, 'vpc-stack', {env:process.env});
 
@@ -32,13 +40,17 @@ export class RootStack extends Stack {
     _DynamoDBStack.addDependency(_VpcStack);
     _DynamoDBStack.addDependency(_OsStack);
 
-    const _LLMStack = new LLMStack(this, 'llm-stack', {_vpc:_VpcStack._vpc, _securityGroup:_VpcStack._securityGroup, _domainEndpoint:_OsStack._domainEndpoint, env:process.env});
-    _LLMStack.addDependency(_VpcStack);
+    // This assest stack is to mitigate issue that the model assets in s3 bucket can't be located immediately to create sagemaker model
+    const _AssetsStack = new AssetsStack(this, 'assets-stack', {_s3ModelAssets:_S3ModelAssets.valueAsString, env:process.env});
+
+    const _LLMStack = new LLMStack(this, 'llm-stack', { _s3ModelAssets:_S3ModelAssets.valueAsString, _crossCodePrefix:_AssetsStack._crossCodePrefix, env:process.env});
+    _LLMStack.addDependency(_AssetsStack);
 
     new CfnOutput(this, 'VPC',{value:_VpcStack._vpc.vpcId});
     new CfnOutput(this, 'OpenSearch Endpoint',{value:_OsStack._domainEndpoint});
     // contatenate the outputs from the ec2 stack with port 8081 and prefix _dashboards
     new CfnOutput(this, 'OpenSearch Dashboard',{value:`${_Ec2Stack._publicIP}:8081/_dashboards`});
+    new CfnOutput(this, 'Cross Model Endpoint',{value:_LLMStack._crossEndPointName || 'No Endpoint Created'});
   }
 }
 
