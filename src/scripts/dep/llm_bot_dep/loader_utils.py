ea2680e06267116d578767dbc34c9367eb8850f8
Ning <lvning19860909@gmail.com>
2023-10-29 20:38:48 +0800

    feat: add CSV loader to save the content in markdown format

--- src/scripts/dep/llm_bot_dep/loader_utils.py
+++ src/scripts/dep/llm_bot_dep/loader_utils.py
@@ -1,9 +1,14 @@
 import logging
 import subprocess
 from pathlib import Path
-from typing import Dict, List, Optional, Iterator
+from typing import Dict, List, Optional, Iterator, Sequence
 from langchain.document_loaders.pdf import BasePDFLoader
+from langchain.document_loaders.csv_loader import CSVLoader
 from langchain.docstore.document import Document
+import csv
+from io import TextIOWrapper
+from langchain.document_loaders.helpers import detect_file_encodings
+
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -22,6 +27,7 @@ class NougatPDFLoader(BasePDFLoader):
         ImportError: If the `nougat` library is not installed.
         RuntimeError: If the `nougat` command fails to execute successfully.
     """
+
     def __init__(self, file_path: str, *, headers: Optional[Dict] = None):
         """Initialize with a file path."""
         try:
@@ -96,9 +102,189 @@ class NougatPDFLoader(BasePDFLoader):
         except Exception as e:
             logging.error(f"An error occurred while processing the PDF: {str(e)}")
 
+
+class CustomCSVLoader(CSVLoader):
+    """Load a `CSV` file into a list of Documents.
+
+    Each document represents one row of the CSV file. The rows are converted into markdown format based on row_count.
+
+    Output Example:
+        when row_count = 1, 
+        page_document_1 contains:
+        |index|name|
+        |-|-|
+        |1|Demo1|
+        page_document_2 contains:
+        |index|name|
+        |-|-|
+        |2|Demo2|
+
+        when row_count = 3, 
+        page_document_1 contains:
+        |index|name|
+        |-|-|
+        |1|Demo1|
+        |2|Demo2|
+        |3|Demo3|
+        page_document_2 contains:
+        |index|name|
+        |-|-|
+        |4|Demo4|
+        |5|Demo5|
+        |6|Demo6|
+    """
+
+    def __init__(
+        self,
+        file_path: str,
+        source_column: Optional[str] = None,
+        metadata_columns: Sequence[str] = (),
+        csv_args: Optional[Dict] = None,
+        encoding: Optional[str] = None,
+        autodetect_encoding: bool = False,
+        row_count: int = 1
+    ):
+        """
+
+        Args:
+            file_path: The path to the CSV file.
+            source_column: The name of the column in the CSV file to use as the source.
+              Optional. Defaults to None.
+            metadata_columns: A sequence of column names to use as metadata. Optional.
+            csv_args: A dictionary of arguments to pass to the csv.DictReader.
+              Optional. Defaults to None.
+            encoding: The encoding of the CSV file. Optional. Defaults to None.
+            autodetect_encoding: Whether to try to autodetect the file encoding.
+            row_count: How many row in a page document.
+        """
+        self.row_number = row_count
+        super().__init__(file_path, source_column, metadata_columns,
+                         csv_args, encoding, autodetect_encoding)
+
+    def __read_file(self, csvfile: TextIOWrapper) -> List[Document]:
+        docs = []
+
+        csv_reader = csv.DictReader(csvfile, **self.csv_args)
+        counter = 0
+        for i, row in enumerate(csv_reader):
+            # print(f"i: {i}")
+            # print(f"row: {row}")
+            try:
+                source = (
+                    row[self.source_column]
+                    if self.source_column is not None
+                    else self.file_path
+                )
+            except KeyError:
+                raise ValueError(
+                    f"Source column '{self.source_column}' not found in CSV file."
+                )
+            counter += 1
+
+            if counter % self.row_number == 1:
+                # First row with header and separator
+                header = "|"
+                md_separator = "|"
+                row_content = "|"
+                for k, v in row.items():
+                    header += k + "|"
+                    md_separator += "-|"
+                    row_content += v + "|"
+                row_content += "\n"
+            elif counter % self.row_number == 0:
+                if 1 == self.row_number:
+                    header = "|"
+                    md_separator = "|"
+                    row_content = "|"
+                    for k, v in row.items():
+                        header += k + "|"
+                        md_separator += "-|"
+                        row_content += v + "|"
+                else:
+                    for k, v in row.items():
+                        row_content += v + "|"
+                content = header + "\n" + md_separator + "\n" + row_content
+                print(f"markdown content: {content}")
+
+                metadata = {"source": source, "row": i}
+                for col in self.metadata_columns:
+                    try:
+                        metadata[col] = row[col]
+                    except KeyError:
+                        raise ValueError(
+                            f"Metadata column '{col}' not found in CSV file.")
+                doc = Document(page_content=content, metadata=metadata)
+                docs.append(doc)
+                counter = 0
+            else:
+                for k, v in row.items():
+                    row_content += v + "|"
+                row_content += "\n"
+
+        return docs
+
+    def load(self) -> List[Document]:
+        """Load data into document objects."""
+
+        docs = []
+        try:
+            with open(self.file_path, newline="", encoding=self.encoding) as csvfile:
+                docs = self.__read_file(csvfile)
+        except UnicodeDecodeError as e:
+            if self.autodetect_encoding:
+                detected_encodings = detect_file_encodings(self.file_path)
+                for encoding in detected_encodings:
+                    try:
+                        with open(
+                            self.file_path, newline="", encoding=encoding.encoding
+                        ) as csvfile:
+                            docs = self.__read_file(csvfile)
+                            break
+                    except UnicodeDecodeError:
+                        continue
+            else:
+                raise RuntimeError(f"Error loading {self.file_path}") from e
+        except Exception as e:
+            raise RuntimeError(f"Error loading {self.file_path}") from e
+
+        return docs
+
+
 # local debugging purpose
 # if __name__ == "__main__":
 #     # local pdf file in current folder
 #     loader = NougatPDFLoader('paperSnapshot.pdf')
 #     data = loader.load()
 #     logging.info("text: %s", data)
+
+
+# TODO: Local debug CSV loader, remove it before release
+# if __name__ == "__main__":
+#     import uuid
+#     import boto3
+#     from datetime import datetime
+
+#     s3 = boto3.client('s3')
+#     now = datetime.now()
+#     timestamp_str = now.strftime("%Y%m%d%H%M%S")
+#     print(timestamp_str)
+#     random_uuid = str(uuid.uuid4())[:8]
+#     print(random_uuid)
+
+#     def process_csv(csv_content: str, kwargs):
+#         bucket_name = kwargs['bucket']
+#         key = kwargs['key']
+#         local_path = f'<path>/temp-{timestamp_str}-{random_uuid}.csv'
+#         s3.download_file(bucket_name, key, local_path)
+
+#         # loader = CustomCSVLoader(file_path=local_path, row_count=1)
+#         # loader = CustomCSVLoader(file_path=local_path, row_count=999)
+#         loader = CustomCSVLoader(file_path=local_path, row_count=2)
+#         # loader = CustomCSVLoader(file_path=local_path, row_count=3)
+#         data = loader.load()
+#         # print(data)
+
+#     # TSV
+#     # process_csv("x", {'bucket': '<bucket_name>', 'key': 'athena_results/OrderTable.tsv'})
+#     # CSV
+#     process_csv("x", {'bucket': '<bucket_name>', 'key': 'athena_results/sdps-api-test-s3-key-58h54muj.csv'})
