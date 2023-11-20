6a9f901874d5356a2fffe2432167ce2d52f3903c
Xu Han <nciiproto@gmail.com>
2023-11-20 15:20:09 +0000

    feat: initial push for unstructured pdf using tesseract

---
+++ source/model/etl/code/aikits_utils.py
@@ -0,0 +1,12 @@
+
+
+def lambda_return(statusCode, body):
+    return {
+        'statusCode': statusCode,
+        'headers': {
+            'Access-Control-Allow-Headers': '*',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': '*'
+        },
+        'body': body
+    }
