e7a0e4a6e72e7e88b3272ff5f109291839309c16
Tang Jie <tangjiee@amazon.com>
2023-12-08 09:53:07 +0000

    feat: merge new feature from rag branch.

---
+++ source/lambda/executor/utils/__init__.py
@@ -0,0 +1,1 @@
+from .llm_utils import generate
---
+++ source/lambda/executor/utils/prompt_template.py
@@ -0,0 +1,55 @@
+
+CLAUDE2_RAG_PROMPT_TEMPLTE = """
+Here is a query:
+
+<query>
+{query}
+</query>
+
+Here are some results relevant to the query:
+
+<search_results>
+{context}
+</search results>
+
+Once again, the user's query is:
+
+<query>
+{query}
+</query>
+
+Provide a response between <result> tags.
+"""
+
+CLAUDE2_RAG_CONTEXT_TEMPLATE="""
+<item index="{index}">
+{content}
+</item>
+"""
+
+
+def claude2_rag_template_render(query:str,contexts:list):
+    """use claude2 offical rag prompte template
+
+    Args:
+        query (str): _description_
+        contexts (list): _description_
+    """
+    assert isinstance(contexts,list), contexts
+    context_xmls = []
+    for i,context in enumerate(contexts):
+        context_xml = CLAUDE2_RAG_CONTEXT_TEMPLATE.format(
+            index = i+1,
+            content = context
+        )
+        context_xmls.append(context_xml)
+    
+    context = "\n".join(context_xmls)
+    prompt = CLAUDE2_RAG_PROMPT_TEMPLTE.format(query=query,context=context)
+    return prompt
+
+
+
+
+
+
