771259e67496d268d6fb33f9a30348e6cde9ca21
Ning <lvning19860909@gmail.com>
2023-12-08 15:41:27 +0800

    feat: add streaming api

---
+++ source/infrastructure/lib/shared/utils.ts
@@ -0,0 +1,28 @@
+import * as cdk from 'aws-cdk-lib';
+import * as iam from 'aws-cdk-lib/aws-iam';
+
+
+/**
+ * Create basic lambda policy.
+ *
+ * @param construct
+ * @returns
+ */
+export function createBasicLambdaPolicy(): iam.PolicyDocument {
+    return new iam.PolicyDocument({
+        statements: [
+            new iam.PolicyStatement({
+                actions: ['logs:CreateLogGroup', 'logs:CreateLogStream'],
+                resources: [
+                    `arn:${cdk.Aws.PARTITION}:logs:${cdk.Aws.REGION}:${cdk.Aws.ACCOUNT_ID}:log-group:/aws/lambda/*`
+                ]
+            }),
+            new iam.PolicyStatement({
+                actions: ['logs:PutLogEvents'],
+                resources: [
+                    `arn:${cdk.Aws.PARTITION}:logs:${cdk.Aws.REGION}:${cdk.Aws.ACCOUNT_ID}:log-group:/aws/lambda/*:log-stream:*`
+                ]
+            })
+        ]
+    });
+}
