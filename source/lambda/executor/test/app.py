c316f4d1748be5d75d180ac9759f1e9f01d45625
Ning <lvning19860909@gmail.com>
2024-01-17 15:12:35 +0800

    feat: add load doc ui

--- source/lambda/executor/test/app.py
+++ source/lambda/executor/test/app.py
@@ -1,9 +1,11 @@
 import gradio as gr
 import json
 import requests
+import boto3
 from executor_local_test import generate_answer
 
 doc_dict = {}
+s3 = boto3.client('s3')
 
 
 def load_raw_data():
@@ -81,7 +83,9 @@ def get_answer(query_input):
     )
 
 
-def invoke_etl_online(url_input, s3_bucket_chunk_input, s3_prefix_chunk_input, need_split_dropdown):
+def invoke_etl_online(
+    url_input, s3_bucket_chunk_input, s3_prefix_chunk_input, need_split_dropdown
+):
     request_body = {
         "s3_bucket": s3_bucket_chunk_input,
         "s3_prefix": s3_prefix_chunk_input,
@@ -145,7 +149,22 @@ def invoke_etl_offline(
     return response_json, response_json["step_function_arn"]
 
 
+def load_s3_bucket():
+    s3_buckets = s3.list_buckets()
+    s3_bucket_names = [bucket['Name'] for bucket in s3_buckets['Buckets']]
+
+    return s3_bucket_names
+
+
+def load_s3_doc(s3_bucket_dropdown, s3_prefix_compare):
+    response = s3.get_object(Bucket=s3_bucket_dropdown, Key=s3_prefix_compare)
+    content = response['Body'].read().decode('utf-8')
+    print(content)
+    return content
+
+
 with gr.Blocks() as demo:
+    s3_bucket_name_list = load_s3_bucket()
     gr.Markdown("LLM Bot Debug UI")
     url_input = gr.Text(
         label="Url, eg. https://f2zgexpo47.execute-api.us-east-1.amazonaws.com/v1/"
@@ -199,7 +218,11 @@ with gr.Blocks() as demo:
     with gr.Tab("Data Process Offline"):
         with gr.Row():
             with gr.Column():
-                s3_bucket_input = gr.Textbox(label="S3 bucket name, eg. llm-bot")
+                s3_bucket_input = gr.Dropdown(
+                    choices=s3_bucket_name_list,
+                    label="S3 Bucket",
+                    info="S3 bucket name, eg. llm-bot",
+                )
             with gr.Column():
                 s3_prefix_input = gr.Textbox(
                     label="S3 prefix, eg. demo_folder/demo.pdf"
@@ -247,11 +270,14 @@ with gr.Blocks() as demo:
             ],
             outputs=[status_output_json],
         )
-
     with gr.Tab("Chunk Comparison"):
         with gr.Row():
             with gr.Column():
-                s3_bucket_chunk_input = gr.Textbox(label="S3 bucket name, eg. llm-bot")
+                s3_bucket_chunk_input = gr.Dropdown(
+                    choices=s3_bucket_name_list,
+                    label="S3 Bucket",
+                    info="S3 bucket name, eg. llm-bot",
+                )
             with gr.Column():
                 s3_prefix_chunk_input = gr.Textbox(
                     label="S3 prefix, eg. demo_folder/demo.pdf"
@@ -263,8 +289,8 @@ with gr.Blocks() as demo:
                     info="Wether to split the content as chunks, default is false",
                 )
         process_online_button = gr.Button("Process Online")
-        with gr.Accordion("Solution Result", open=False):
-            solution_md = gr.JSON()
+        with gr.Accordion("Online Result", open=False):
+            online_result = gr.JSON()
         process_online_button.click(
             fn=invoke_etl_online,
             inputs=[
@@ -273,16 +299,35 @@ with gr.Blocks() as demo:
                 s3_prefix_chunk_input,
                 need_split_dropdown,
             ],
+            outputs=[online_result],
+        )
+
+        with gr.Row():
+            with gr.Column():
+                s3_bucket_dropdown = gr.Dropdown(
+                    choices=s3_bucket_name_list,
+                    label="S3 Bucket",
+                    info="S3 bucket name, eg. llm-bot",
+                )
+            with gr.Column():
+                s3_prefix_compare = gr.Textbox(
+                    label="S3 prefix, eg. demo_folder/demo.pdf"
+                )
+        load_button = gr.Button("Load")
+        solution_md = gr.TextArea(label="Output")
+        load_button.click(
+            fn=load_s3_doc,
+            inputs=[
+                s3_bucket_dropdown,
+                s3_prefix_compare,
+            ],
             outputs=[solution_md],
         )
-        # with gr.Row():
-        #     with gr.Column():
-        #         aws_md = gr.Markdown()
-        #     with gr.Column():
-        #         pp_md = gr.Markdown()
-        #     with gr.Column():
-        #         unstructured_md = gr.JSON()
-
+        with gr.Row():
+            with gr.Column():
+                lc_button = gr.Button("Compare with Langchain")
+            with gr.Column():
+                pp_button = gr.Button("Compare with PPStructure")
 
 # load_raw_data()
 if __name__ == "__main__":
