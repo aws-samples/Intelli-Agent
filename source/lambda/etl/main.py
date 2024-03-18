2046b55041c576dd0244d601acec8b26ad6b49e1
Xu Han <nciiproto@gmail.com>
2024-03-18 13:50:57 +0000

    feat: support offline job trigger by s3

--- source/lambda/etl/main.py
+++ source/lambda/etl/main.py
@@ -32,40 +32,64 @@ def lambda_handler(event, context):
     index_type = event.get("indexType", "qd")
     operation_type = event.get("operationType", "create")
 
-    # Initialize the file count
-    file_count = 0
+    if "offline" not in event:
+        raise ValueError("offline is not in the event")
+    elif event["offline"].lower() == "true":
 
-    # Paginate through the list of objects in the bucket with the specified prefix
-    paginator = s3_client.get_paginator("list_objects_v2")
-    page_iterator = paginator.paginate(Bucket=bucket_name, Prefix=prefix)
+        # Initialize the file count
+        file_count = 0
 
-    # Count the files, note skip the prefix with slash, which is the folder name
-    for page in page_iterator:
-        for obj in page.get("Contents", []):
-            key = obj["Key"]
-            file_type = key.split(".")[-1].lower()  # Extract file extension
-            if key.endswith("/") or file_type not in supported_file_types:
-                continue
+        # Paginate through the list of objects in the bucket with the specified prefix
+        paginator = s3_client.get_paginator("list_objects_v2")
+        page_iterator = paginator.paginate(Bucket=bucket_name, Prefix=prefix)
 
-            file_count += 1
+        # Count the files, note skip the prefix with slash, which is the folder name
+        for page in page_iterator:
+            for obj in page.get("Contents", []):
+                key = obj["Key"]
+                file_type = key.split(".")[-1].lower()  # Extract file extension
+                if key.endswith("/") or file_type not in supported_file_types:
+                    continue
 
-    job_number = get_job_number(event, file_count)
+                file_count += 1
+        file_count = 1 if file_count == 0 else file_count
+        job_number = get_job_number(event, file_count)
 
-    batch_file_number = (file_count - 1) // job_number + 1
+        batch_file_number = (file_count - 1) // job_number + 1
 
-    # convert the fileCount into an array of numbers "fileIndices": [0, 1, 2, ..., 10], an array from 0 to fileCount-1
-    batch_indices = list(range(job_number))
+        # convert the fileCount into an array of numbers "fileIndices": [0, 1, 2, ..., 10], an array from 0 to fileCount-1
+        batch_indices = list(range(job_number))
 
-    # This response should match the expected input schema of the downstream tasks in the Step Functions workflow
-    return {
-        "s3Bucket": bucket_name,
-        "s3Prefix": prefix,
-        "fileCount": file_count,
-        "workspaceId": workspace_id,
-        "qaEnhance": event["qaEnhance"].lower() if "qaEnhance" in event else "false",
-        "offline": event["offline"].lower(),
-        "batchFileNumber": str(batch_file_number),
-        "batchIndices": batch_indices,
-        "indexType": index_type,
-        "operationType": operation_type,
-    }
+        # This response should match the expected input schema of the downstream tasks in the Step Functions workflow
+        return {
+            "s3Bucket": bucket_name,
+            "s3Prefix": prefix,
+            "fileCount": file_count,
+            "workspaceId": workspace_id,
+            "qaEnhance": (
+                event["qaEnhance"].lower() if "qaEnhance" in event else "false"
+            ),
+            "offline": event["offline"].lower(),
+            "batchFileNumber": str(batch_file_number),
+            "batchIndices": batch_indices,
+            "indexType": index_type,
+            "operationType": operation_type,
+        }
+    elif event["offline"].lower() == "false":
+        # This response should match the expected input schema of the downstream tasks in the Step Functions workflow
+        return {
+            "s3Bucket": bucket_name,
+            "s3Prefix": prefix,
+            "fileCount": "1",
+            "workspaceId": workspace_id,
+            "qaEnhance": (
+                event["qaEnhance"].lower() if "qaEnhance" in event else "false"
+            ),
+            "offline": "false",
+            "batchFileNumber": "1",
+            "batchIndices": "0",
+            "indexType": index_type,
+            "operationType": operation_type,
+        }
+    else:
+        raise ValueError("offline is not true or false")
