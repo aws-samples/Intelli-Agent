37e7aadc36e3884763066dd381b3d16f508bf7a2
zhouxss <zhouxss@amazon.com>
2024-03-22 06:51:55 +0000

    modify market_conversation_summary_entry.py

--- source/lambda/executor/utils/parse_config.py
+++ source/lambda/executor/utils/parse_config.py
@@ -17,77 +17,77 @@ def update_nest_dict(d, u):
 
 
 # default rag config
-rag_default_config = {
-    # retriver config
-    # query process config
-    "retriever_config": {
-        "retriever_top_k": 5,
-        "chunk_num": 2,
-        "using_whole_doc": False,
-        "reranker_top_k": 10,
-        "reranker_type": RerankerType.BYPASS.value,
-        "q_q_match_threshold": 0.8,
-        "workspace_ids": [],
-    },
-    "query_process_config": {
-        "query_rewrite_config": {
-            "model_id": "anthropic.claude-instant-v1",
-        },
-        "conversation_query_rewrite_config": {
-            "model_id": "anthropic.claude-instant-v1",
-        },
-        "hyde_config": {
-            "model_id": "anthropic.claude-instant-v1",
-        },
-        "stepback_config": {
-            "model_id": "anthropic.claude-instant-v1",
-        },
-        "translate_config": {
-            # default use Amazon Translate service
-            "model_id": AWS_TRANSLATE_SERVICE_MODEL_ID
-        },
-    },
-    # intent_config
-    "intent_config": {
-        "intent_type": IntentType.KNOWLEDGE_QA.value,
-        "model_id": "anthropic.claude-v2:1",
-        "sub_intent": {},
-    },
-    # generator config
-    "generator_llm_config": {
-        "model_id": "anthropic.claude-v2:1",
-        "context_num": 2,
-    },
-    "mkt_conversation_summary_config": {
-        "model_id": "anthropic.claude-v2:1",
-    },
-    "debug_level": logging.INFO,
-    "session_id": None,
-    "ws_connection_id": None,
-    "chat_history": None,
-}
+# rag_default_config = {
+#     # retriver config
+#     # query process config
+#     "retriever_config": {
+#         "retriever_top_k": 5,
+#         "chunk_num": 2,
+#         "using_whole_doc": False,
+#         "reranker_top_k": 10,
+#         "reranker_type": RerankerType.BYPASS.value,
+#         "q_q_match_threshold": 0.8,
+#         "workspace_ids": [],
+#     },
+#     "query_process_config": {
+#         "query_rewrite_config": {
+#             "model_id": "anthropic.claude-instant-v1",
+#         },
+#         "conversation_query_rewrite_config": {
+#             "model_id": "anthropic.claude-instant-v1",
+#         },
+#         "hyde_config": {
+#             "model_id": "anthropic.claude-instant-v1",
+#         },
+#         "stepback_config": {
+#             "model_id": "anthropic.claude-instant-v1",
+#         },
+#         "translate_config": {
+#             # default use Amazon Translate service
+#             "model_id": AWS_TRANSLATE_SERVICE_MODEL_ID
+#         },
+#     },
+#     # intent_config
+#     "intent_config": {
+#         "intent_type": IntentType.KNOWLEDGE_QA.value,
+#         "model_id": "anthropic.claude-v2:1",
+#         "sub_intent": {},
+#     },
+#     # generator config
+#     "generator_llm_config": {
+#         "model_id": "anthropic.claude-v2:1",
+#         "context_num": 2,
+#     },
+#     "mkt_conversation_summary_config": {
+#         "model_id": "anthropic.claude-v2:1",
+#     },
+#     "debug_level": logging.INFO,
+#     "session_id": None,
+#     "ws_connection_id": None,
+#     "chat_history": None,
+# }
 
 
-def parse_rag_config(event_body):
-    event_body = copy.deepcopy(event_body)
-    new_event_config = update_nest_dict(copy.deepcopy(rag_default_config), event_body)
+# def parse_rag_config(event_body):
+#     event_body = copy.deepcopy(event_body)
+#     new_event_config = update_nest_dict(copy.deepcopy(rag_default_config), event_body)
 
-    # adapting before setting
-    temperature = event_body.get("temperature")
-    llm_model_id = event_body.get("llm_model_id")
+#     # adapting before setting
+#     temperature = event_body.get("temperature")
+#     llm_model_id = event_body.get("llm_model_id")
 
-    if llm_model_id:
-        new_event_config["generator_llm_config"]["model_id"] = llm_model_id
-    if temperature:
-        new_event_config["generator_llm_config"]["model_kwargs"][
-            "temperature"
-        ] = temperature
+#     if llm_model_id:
+#         new_event_config["generator_llm_config"]["model_id"] = llm_model_id
+#     if temperature:
+#         new_event_config["generator_llm_config"]["model_kwargs"][
+#             "temperature"
+#         ] = temperature
 
-    intent = event_body.get("intent", None) or event_body.get("model", None)
-    if intent:
-        new_event_config["intent_config"]["intent_type"] = intent
+#     intent = event_body.get("intent", None) or event_body.get("model", None)
+#     if intent:
+#         new_event_config["intent_config"]["intent_type"] = intent
 
-    return new_event_config
+#     return new_event_config
 
 def parse_sagemind_llm_config(event_body):
     event_body = copy.deepcopy(event_body)
