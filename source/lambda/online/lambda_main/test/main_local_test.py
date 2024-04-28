e13028e84a2541582ae320109766cf2a5f3d8954
AoyuQC <aoyuzhang1989@gmail.com>
2024-04-28 02:03:37 +0000

    feat: refactor on line lambda

---
+++ source/lambda/online/lambda_main/test/main_local_test.py
@@ -0,0 +1,123 @@
+import json
+import sys
+import csv
+import os 
+import time 
+import uuid
+
+from dotenv import load_dotenv
+# load_dotenv(
+#     dotenv_path=os.path.join(os.path.dirname(__file__),'.env_global_430')
+# )
+
+# load_dotenv(
+#     dotenv_path=os.path.join(os.path.dirname(__file__),'.env_global_430')
+# )
+
+import logging
+log_level = logging.INFO
+logging.basicConfig(
+    level=log_level,
+    format='%(asctime)s %(levelname)s [%(name)s] %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+)
+logger = logging.getLogger(__name__)
+
+sys.path.append("./lambda_main")
+import main
+import os
+from collections import defaultdict
+
+contexts = defaultdict(str)
+
+class DummyWebSocket:
+    def post_to_connection(self,ConnectionId,Data):
+        data = json.loads(Data)
+        ret = data
+        message_type = ret['choices'][0]['message_type']
+        if message_type == "START":
+            pass
+        elif message_type == "CHUNK":
+            print(ret['choices'][0]['message']['content'],end="",flush=True)
+        elif message_type == "END":
+            return 
+        elif message_type == "ERROR":
+            print(ret['choices'][0]['message']['content'])
+            return 
+        elif message_type == "CONTEXT":
+            # print(ret['choices'][0])
+            message:dict = ret['choices'][0]
+            if "_chunk_data" in ret['choices'][0]:
+                contexts[message['message_id']] += message['_chunk_data']
+                if message["chunk_id"] + 1 != message['total_chunk_num']:
+                    return 
+                _chunk_data = contexts.pop(message['message_id'])
+                print('context chunk num',message['total_chunk_num'])
+                message.update(json.loads(_chunk_data))
+            
+            print('knowledge_sources',message['knowledge_sources'])
+            print('response msg',message['response_msg'])
+            print(message.keys())
+
+main.ws_client = DummyWebSocket()
+
+def generate_answer(query=None,
+                    messages=None,
+                    # temperature=0.7,
+                    enable_debug=True,
+                    retrieval_only=False,
+                    type="common",
+                    model="knowledge_qa",
+                    stream=False,
+                    retriever_index="test-index",
+                    session_id=None,
+                    rag_parameters=None
+                    ):
+    rag_parameters = rag_parameters or {}
+
+    if query:
+        messages = [
+                {
+                    "role": "user",
+                    "content": query
+                }
+            ]
+    else:
+        assert messages and isinstance(messages,list), messages
+
+    body = {
+            "messages": messages,
+            # "temperature": temperature,
+            # "enable_debug": enable_debug,
+            # "retrieval_only": retrieval_only,
+            # "retriever_index": retriever_index,
+            "type": type,
+            "model": model,
+            "session_id":session_id,
+            "enable_debug":True,
+            }
+    body.update(rag_parameters)
+    event = {
+        "body": json.dumps(body)
+    }
+    if stream:
+        event["requestContext"] = {
+            "eventType":"MESSAGE",
+            "connectionId":f'test_{int(time.time())}'
+        }
+
+    context = None
+    response = main.lambda_handler(event, context)
+    if response is None:
+        return
+    if not stream:
+        body = json.loads(response["body"])
+        answer = body["choices"][0]["message"]["content"]
+        knowledge_sources = body["choices"][0]["message"]["knowledge_sources"]
+        debug_info = body["debug_info"]
+        return (answer,
+                knowledge_sources,
+                debug_info)
+
+if __name__ == "__main__":
+    generate_answer("ECS容器中的日志，可以配置输出到S3上吗？")
---
+++ source/lambda/online/utils/response_utils.py
@@ -0,0 +1,268 @@
+import copy
+import csv
+import json
+import logging
+import os
+import time
+import traceback
+
+from .constant import EntryType, StreamMessageType
+from .content_filter_utils.content_filters import (
+    MarketContentFilter,
+    token_to_sentence_gen_market,
+)
+
+logger = logging.getLogger("response_utils")
+
+# marketing
+market_content_filter = MarketContentFilter()
+
+
+class WebsocketClientError(Exception):
+    pass
+
+
+def api_response(**kwargs):
+    response = {"statusCode": 200, "headers": {"Content-Type": "application/json"}}
+    session_id = kwargs["session_id"]
+    entry_type = kwargs["entry_type"]
+    # model = kwargs["model"]
+    request_timestamp = kwargs["request_timestamp"]
+    answer = kwargs["answer"]
+    sources = kwargs["sources"]
+    get_contexts = kwargs["get_contexts"]
+    contexts = kwargs["contexts"]
+    enable_debug = kwargs["enable_debug"]
+    debug_info = kwargs["debug_info"]
+    ddb_history_obj = kwargs["ddb_history_obj"]
+    message_id = kwargs["message_id"]
+    question = kwargs["question"]
+    client_type = kwargs["client_type"]
+    custom_message_id = kwargs["custom_message_id"]
+
+    if not isinstance(answer, str):
+        answer = json.dumps(answer, ensure_ascii=False)
+
+    if entry_type != EntryType.MARKET_CONVERSATION_SUMMARY.value:
+        ddb_history_obj.add_user_message(
+            f"user_{message_id}", custom_message_id, entry_type, question
+        )
+        ddb_history_obj.add_ai_message(
+            f"ai_{message_id}",
+            custom_message_id,
+            entry_type,
+            answer,
+            input_message_id=f"user_{message_id}",
+        )
+
+    # 2. return rusult
+    llmbot_response = {
+        "session_id": session_id,
+        "client_type": client_type,
+        "object": "chat.completion",
+        "created": int(request_timestamp),
+        # "model": model,
+        # "usage": {"prompt_tokens": 13, "completion_tokens": 7, "total_tokens": 20},
+        "choices": [
+            {
+                "message": {
+                    "role": "assistant",
+                    "content": answer,
+                    "knowledge_sources": sources,
+                },
+                "message_id": f"ai_{message_id}",
+                "custom_message_id": custom_message_id,
+                "finish_reason": "stop",
+                "index": 0,
+            }
+        ],
+        "entry_type": entry_type,
+    }
+
+    resp_header = {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "*",
+    }
+    if get_contexts:
+        llmbot_response["contexts"] = contexts
+    if enable_debug:
+        debug_info["contexts"] = contexts
+        llmbot_response["debug_info"] = debug_info
+    response["body"] = json.dumps(llmbot_response)
+    response["headers"] = resp_header
+
+    return response
+
+
+def stream_response(**kwargs):
+    session_id = kwargs["session_id"]
+    # model = kwargs.["model"]
+    request_timestamp = kwargs["request_timestamp"]
+    answer = kwargs["answer"]
+    entry_type = kwargs["entry_type"]
+    sources = kwargs["sources"]
+    get_contexts = kwargs["get_contexts"]  # bool
+    contexts = kwargs["contexts"]  # retrieve result
+    enable_debug = kwargs["enable_debug"]
+    debug_info = kwargs["debug_info"]
+    ws_client = kwargs["ws_client"]
+    ddb_history_obj = kwargs["ddb_history_obj"]
+    message_id = kwargs["message_id"]
+    question = kwargs["question"]
+
+    ws_connection_id = kwargs["ws_connection_id"]
+    log_first_token_time = kwargs.get("log_first_token_time", True)
+    client_type = kwargs["client_type"]
+    custom_message_id = kwargs["custom_message_id"]
+    main_entry_end = kwargs["main_entry_end"]
+
+    if isinstance(answer, str):
+        answer = iter([answer])
+
+    def _stop_stream():
+        pass
+        # if not isinstance(answer,list):
+        #     answer.close()
+
+    def _send_to_ws_client(message: dict):
+        try:
+            llmbot_response = {
+                "session_id": session_id,
+                "client_type": client_type,
+                "object": "chat.completion",
+                "created": int(request_timestamp),
+                "choices": [message],
+                "entry_type": entry_type,
+            }
+            ws_client.post_to_connection(
+                ConnectionId=ws_connection_id,
+                Data=json.dumps(llmbot_response).encode("utf-8"),
+            )
+        except:
+            data_to_send = json.dumps(llmbot_response).encode("utf-8")
+            logger.info(
+                f"Send to ws client error occurs, the message to send is: {data_to_send}"
+            )
+            # convert to websocket error
+            raise WebsocketClientError
+
+    try:
+        _send_to_ws_client(
+            {
+                "message_type": StreamMessageType.START,
+                "message_id": f"ai_{message_id}",
+                "custom_message_id": custom_message_id,
+            }
+        )
+        answer_str = ""
+
+        filter_sentence_fn = lambda x: x
+        if market_content_filter.check_market_entry(entry_type):
+            answer = token_to_sentence_gen_market(answer)
+            filter_sentence_fn = market_content_filter.filter_sentence
+            sources = market_content_filter.filter_source(kwargs["sources"])
+
+        for i, chunk in enumerate(answer):
+            if i == 0 and log_first_token_time:
+                first_token_time = time.time()
+                logger.info(
+                    f"{custom_message_id} running time of first token generated {entry_type} : {first_token_time-main_entry_end}s"
+                )
+                logger.info(
+                    f"{custom_message_id} running time of first token whole {entry_type} : {first_token_time-request_timestamp}s"
+                )
+            chunk = filter_sentence_fn(chunk)
+            _send_to_ws_client(
+                {
+                    "message_type": StreamMessageType.CHUNK,
+                    "message_id": f"ai_{message_id}",
+                    "custom_message_id": custom_message_id,
+                    "message": {
+                        "role": "assistant",
+                        "content": chunk,
+                        # "knowledge_sources": sources,
+                    },
+                    "chunk_id": i,
+                }
+            )
+
+            answer_str += chunk
+
+        if log_first_token_time:
+            logger.info(
+                f"{custom_message_id} running time of last token whole {entry_type} : {time.time()-request_timestamp}s"
+            )
+
+        logger.info(f"answer: {answer_str}")
+        # add to chat history ddb table
+        if entry_type != EntryType.MARKET_CONVERSATION_SUMMARY.value:
+            ddb_history_obj.add_user_message(
+                f"user_{message_id}", custom_message_id, entry_type, question
+            )
+            ddb_history_obj.add_ai_message(
+                f"ai_{message_id}",
+                custom_message_id,
+                entry_type,
+                answer_str,
+                input_message_id=f"user_{message_id}",
+            )
+        # sed source and contexts
+
+        context_msg = {
+            "message_type": StreamMessageType.CONTEXT,
+            "message_id": f"ai_{message_id}",
+            "custom_message_id": custom_message_id,
+            "knowledge_sources": sources,
+        }
+        if get_contexts:
+            context_msg.update({"contexts": contexts})
+
+        if enable_debug:
+            debug_info["stream_full_answer"] = answer_str
+            context_msg.update({"debug_info": debug_info})
+
+        _send_to_ws_client(context_msg)
+        # send end
+        _send_to_ws_client(
+            {
+                "message_type": StreamMessageType.END,
+                "message_id": f"ai_{message_id}",
+                "custom_message_id": custom_message_id,
+            }
+        )
+    except WebsocketClientError:
+        error = traceback.format_exc()
+        logger.info(error)
+        _stop_stream()
+    except:
+        # bedrock error
+        error = traceback.format_exc()
+        logger.info(error)
+        _send_to_ws_client(
+            {
+                "message_type": StreamMessageType.ERROR,
+                "message_id": f"ai_{message_id}",
+                "custom_message_id": custom_message_id,
+                "message": {"content": error},
+            }
+        )
+
+
+class WebSocketCallback:
+    def __init__(self, **kwargs):
+        self.kwargs = kwargs
+
+    def __call__(self, answer, contexts):
+        kwargs = {"answer": answer, "contexts": contexts}
+        kwargs.update(**self.kwargs)
+
+        return stream_response(**kwargs)
+
+
+def process_response(**kwargs):
+    stream = kwargs["stream"]
+    if stream:
+        return stream_response(**kwargs)
+    return api_response(**kwargs)
