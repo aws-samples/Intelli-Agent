4486c21583739e4b52315f08b7064a8e23cb91a2
Magic Chen <haiyunc@amazon.com>
2024-05-06 15:45:41 +0800

    chore: add i18n

--- source/portal/src/main.tsx
+++ source/portal/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import '@cloudscape-design/global-styles/index.css';
 import './index.scss';
+import './i18n';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
