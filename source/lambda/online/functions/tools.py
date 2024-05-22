ceec309a143074a3fd64d3e523382204fbd26dc4
zhouxss <zhouxss@amazon.com>
2024-05-22 06:03:20 +0000

    add tools from yuanbo

--- source/lambda/online/functions/tools.py
+++ source/lambda/online/functions/tools.py
@@ -135,6 +135,131 @@ tool_manager.register_tool(
 
 
 
+##### yuanbo default tools #########
+tool_manager.register_tool({
+    "name": "service_availability",
+    "lambda_name": "xxxx",
+    "lambda_module_path": "xxxx",
+    "tool_def":{
+        "name": "service_availability",
+        "description":"query the availability of service in specified region",
+        "parameters":{
+            "type":"object",
+            "properties":{
+                "service":{
+                    "type":"string",
+                    "description":"the AWS service name"
+                },
+                "region":{
+                    "type":"string",
+                    "description":"the AWS region name where the service is located in, for example us-east-1(N.Virginal), us-west-2(Oregon), eu-west-2(London), ap-southeast-1(Singapore)"
+                }
+            },
+            "required":[
+                "service",
+                "region"
+            ]
+        }
+    }
+})
+
+
+tool_manager.register_tool({
+    "name": "explain_abbr",
+    "lambda_name": "xxxx",
+    "lambda_module_path": "xxxx",
+    "tool_def":{
+        "name": "explain_abbr",
+        "description": "explain abbreviation for user",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "abbr": {
+                    "type": "string",
+                    "description": "the abbreviation of terms in AWS"
+                }
+            },
+            "required": ["abbr"]
+        }
+    }
+})
+
+tool_manager.register_tool({
+    "name": "get_contact",
+    "lambda_name": "xxxx",
+    "lambda_module_path": "xxxx",
+    "tool_def":{
+        "name":"get_contact",
+        "description":"query the contact person in the 'SSO' organization",
+        "parameters":{
+            "type":"object",
+            "properties":{
+                "employee":{
+                    "type":"string",
+                    "description":"employee name in the 'SSO' organization"
+                },
+                "role":{
+                    "type":"string",
+                    "description":"employee's role, usually it's Sales, Product Manager, Tech, Program Manager, Leader"
+                },
+                "domain":{
+                    "type":"string",
+                    "description":"Techical domain for the employee，For Example AIML, Analytics, Compute"
+                },
+                "scope":{
+                    "type":"string",
+                    "description":"employee's scope of responsibility. For Sales role, it could be territory like north/east/south/west, For tech role, it could be specific service"
+                }
+            },
+            "required":[
+                "employee"
+            ]
+        }
+     }
+})
+
+tool_manager.register_tool({
+    "name": "ec2_price",
+    "lambda_name": "xxxx",
+    "lambda_module_path": "xxxx",
+    "tool_def": {
+        "name": "ec2_price",
+        "description": "query the price of AWS ec2 instance",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "instance_type": {
+                    "type": "string",
+                    "description": "the AWS ec2 instance type, for example, c5.xlarge, m5.large, t3.mirco, g4dn.2xlarge, if it is a partial of the instance type, you should try to auto complete it. for example, if it is r6g.2x, you can complete it as r6g.2xlarge"
+                },
+                "region": {
+                    "type": "string",
+                    "description": "the AWS region name where the ec2 is located in, for example us-east-1, us-west-1, if it is common words such as 'us east 1','美东1','美西2',you should try to normalize it to standard AWS region name, for example, 'us east 1' is normalized to 'us-east-1', '美东2' is normalized to 'us-east-2','美西2' is normalized to 'us-west-2','北京' is normalized to 'cn-north-1', '宁夏' is normalized to 'cn-northwest-1', '中国区' is normalized to 'cn-north-1'"
+                },
+                "os": {
+                    "type": "string",
+                    "description": "the operating system of ec2 instance, the valid value should be 'Linux' or 'Windows'"
+                },
+                "term": {
+                    "type": "string",
+                    "description": "the payment term, the valid value should be 'OnDemand' or 'Reserved' "
+                },
+                "purchase_option": {
+                    "type": "string",
+                    "description": "the purchase option of Reserved instance, the valid value should be 'No Upfront', 'Partial Upfront' or 'All Upfront' "
+                }
+            },
+            "required": ["instance_type"]
+        }
+    }
+
+})
+
+
+
+
+
+
 
     
 
