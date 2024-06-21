c12a7fb87f40ce9ea48417728f3d3534367f262a
Tang Jie <outstandingcandy@gmail.com>
2024-06-21 08:30:14 +0000

    feat: add delete and upload s3 file utils.

--- source/lambda/online/common_logic/common_utils/s3_utils.py
+++ source/lambda/online/common_logic/common_utils/s3_utils.py
@@ -11,3 +11,10 @@ def download_dir_from_s3(bucket_name, s3_dir_path, local_dir_path):
                     os.makedirs(os.path.dirname(local_dir_path + os.sep + file.get('Key')))
                 s3.download_file(bucket_name, file.get('Key'), local_dir_path + os.sep + file.get('Key'))
 
+def delete_s3_file(bucket_name, s3_file_path):
+    s3 = boto3.client('s3')
+    s3.delete_object(Bucket=bucket_name, Key=s3_file_path)
+
+def upload_file_to_s3(bucket_name, s3_file_path, local_file_path):
+    s3 = boto3.client('s3')
+    s3.upload_file(local_file_path, bucket_name, s3_file_path)
