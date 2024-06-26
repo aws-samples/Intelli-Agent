3a51b9261e3cbbb3ef9701b5ad52216f5998c3cd
Tang Jie <outstandingcandy@gmail.com>
2024-06-26 05:41:58 +0000

    feat: add s3 file download func.

--- source/lambda/online/common_logic/common_utils/s3_utils.py
+++ source/lambda/online/common_logic/common_utils/s3_utils.py
@@ -11,6 +11,10 @@ def download_dir_from_s3(bucket_name, s3_dir_path, local_dir_path):
                     os.makedirs(os.path.dirname(local_dir_path + os.sep + file.get('Key')))
                 s3.download_file(bucket_name, file.get('Key'), local_dir_path + os.sep + file.get('Key'))
 
+def download_file_from_s3(bucket_name, s3_file_path, local_file_path):
+    s3 = boto3.client('s3')
+    s3.download_file(bucket_name, s3_file_path, local_file_path)
+
 def delete_s3_file(bucket_name, s3_file_path):
     s3 = boto3.client('s3')
     s3.delete_object(Bucket=bucket_name, Key=s3_file_path)
