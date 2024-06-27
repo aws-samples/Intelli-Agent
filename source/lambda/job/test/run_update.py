2ce41732edc6d6e55d833274396e1883d9336688
Tang Jie <outstandingcandy@gmail.com>
2024-06-27 13:20:19 +0000

    feat: add goods info index.

--- source/lambda/job/test/run_update.py
+++ source/lambda/job/test/run_update.py
@@ -7,19 +7,24 @@ process_number = 1
 batch_file_number = 3000
 embedding_model_endpoint = "embedding-and-reranker-bce-embedding-and-bge-reranker-43972"
 s3_bucket = "aws-chatbot-knowledge-base-test"
-s3_prefix_list = ["retail/quick_reply/quick_reply_ingestion_data.jsonl", "retail/intent_data/intent_ingestion_data.jsonl", "retail/quick_reply/quick_reply_ingestion_data.jsonl", "retail/quick_reply/shouhou_wuliu.jsonl"]
-workspace_id_list = ["retail-quick-reply", "retail-intent", "retail-shouhou-wuliu", "yb_intent"]
-index_type_list = ["qq", "qq", "qq", "qq"]
-op_type_list = ["update", "update", "update", "update"]
+s3_prefix_list = ["retail/quick_reply/quick_reply_ingestion_data.jsonl",
+                  "retail/intent_data/intent_ingestion_data.jsonl",
+                  "retail/quick_reply/quick_reply_ingestion_data.jsonl",
+                  "retail/goods_info/goods_info.jsonl",
+                  "demo/yb_aos_examples.jsonl"]
+workspace_id_list = ["retail-quick-reply", "retail-intent", "retail-shouhou-wuliu", "goods_info", "yb_intent"]
+index_type_list = ["qq", "qq", "qq", "qq", "qq"]
+op_type_list = ["update", "update", "update", "update", "update"]
 local_file_list = ["poc/goods_data/quick_reply/quick_reply_ingestion_data.jsonl",
                    "poc/intent_data/intent_ingestion_data.jsonl",
                    "poc/goods_data/quick_reply/shouhou_wuliu.jsonl",
+                   "poc/goods_data/detail/goods_info.jsonl",
                    "yb_aos_examples.jsonl"]
 
-workspace_id_list = workspace_id_list[:-1]
-index_type_list = index_type_list[:-1]
-op_type_list = op_type_list[:-1]
-local_file_list = local_file_list[:-1]
+workspace_id_list = workspace_id_list[3:4]
+index_type_list = index_type_list[3:4]
+op_type_list = op_type_list[3:4]
+local_file_list = local_file_list[3:4]
 
 for s3_prefix, workspace_id, index_type, local_file in zip(s3_prefix_list, workspace_id_list, index_type_list, local_file_list):
     s3_utils.delete_s3_file(s3_bucket, s3_prefix)
