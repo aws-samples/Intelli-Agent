4733a307eb2ea4808d2757116109ae2497d3f9a6
AoyuQC <aoyuzhang1989@gmail.com>
2024-07-03 05:58:43 +0000

    finish complete test

--- source/lambda/online/lambda_main/test/main_local_test_common.py
+++ source/lambda/online/lambda_main/test/main_local_test_common.py
@@ -28,7 +28,6 @@ def test_multi_turns_rag_pr():
     session_id = f"multiturn_test_{time.time()}"
     user_queries = [
         {"query":"什么是aws ec2", "use_history":True},
-        {"query":"今天天气怎么样", "use_history":True},
         {"query":"什么是sagemaker", "use_history":True},
     ]
 
@@ -51,10 +50,8 @@ def test_multi_turns_chat_pr():
     mode="chat"
     session_id = f"multiturn_test_{time.time()}"
     user_queries = [
-        {"query":"请用中文回答 what if the customer has EBS requirement", "use_history":True},
         {"query":"你好", "use_history":True},
-        {"query":"What's the weather today?", "use_history":True},
-        {"query":"我在上海", "use_history":True},
+        {"query":"你今天心情如何", "use_history":True},
     ]
 
     for query in user_queries:
@@ -73,7 +70,7 @@ def test_multi_turns_chat_pr():
 def test_multi_turns_agent_pr():
     print("complete test for agent")
     print("++"*50)
-    mode="rag"
+    mode="agent"
     session_id = f"multiturn_test_{time.time()}"
     user_queries = [
         {"query":"你好", "use_history":True},
@@ -96,16 +93,16 @@ def test_multi_turns_agent_pr():
         print()
 
 def complete_test_pr():
+    print("start test in agent mode")
+    test_multi_turns_agent_pr()
+    print("finish test in agent mode")
+    print("start test in rag mode")
+    test_multi_turns_rag_pr()
+    print("finish test in rag mode")
     print("start test in chat mode")
     test_multi_turns_chat_pr()
     # print(srg)
     print("finish test in chat mode")
-    print("start test in rag mode")
-    test_multi_turns_rag_pr()
-    print("finish test in rag mode")
-    print("start test in agent mode")
-    test_multi_turns_agent_pr()
-    print("finish test in agent mode")
   
 if __name__ == "__main__":
     complete_test_pr()
