992f7d82e94a877abd4b7a436c3be08c7e06fe74
NingLu <lvning19860909@gmail.com>
2024-08-14 03:49:27 +0000

    feat: support qa from connect

--- source/lambda/online/lambda_main/main.py
+++ source/lambda/online/lambda_main/main.py
@@ -14,6 +14,7 @@ from common_logic.common_utils.lambda_invoke_utils import (
 )
 from botocore.exceptions import ClientError
 from datetime import datetime, timezone
+from utils.response_utils import process_response
 
 
 logger = get_logger("main")
@@ -36,6 +37,9 @@ chatbot_table = dynamodb.Table(os.environ.get("CHATBOT_TABLE_NAME"))
 model_table = dynamodb.Table(os.environ.get("MODEL_TABLE_NAME"))
 embedding_endpoint = os.environ.get("EMBEDDING_ENDPOINT")
 create_time = str(datetime.now(timezone.utc))
+connect_client = boto3.client("connectcases")
+connect_domain_id = os.environ.get("CONNECT_DOMAIN_ID", "")
+connect_user_arn = os.environ.get("CONNECT_USER_ARN", "")
 
 
 def get_secret_value(secret_arn: str):
@@ -61,9 +65,121 @@ def get_secret_value(secret_arn: str):
             raise Exception("Fail to retrieve the secret value")
 
 
+def connect_case_event_handler(event_body: dict, context: dict, executor):
+    performed_by = event_body["detail"]["performedBy"]["iamPrincipalArn"]
+    logger.info(performed_by)
+    if "AWSServiceRoleForAmazonConnect" not in performed_by:
+        return None
+
+    executor_body = compose_connect_body(event_body, context)
+
+    try:
+        response: dict = executor.run(executor_body)
+        r = process_response(executor_body, response)
+
+        response_message = r["message"]["content"]
+        logger.info(response_message)
+        logger.info("Add response message to case comment")
+
+        related_item = event_body["detail"]["relatedItem"]
+        case_id = related_item["caseId"]
+
+        response = connect_client.create_related_item(
+            caseId=case_id,
+            content={
+                "comment": {"body": response_message, "contentType": "Text/Plain"},
+            },
+            domainId=connect_domain_id,
+            performedBy={
+                "userArn": connect_user_arn,
+            },
+            type="Comment",
+        )
+        logger.info(response)
+    except Exception as e:
+        msg = traceback.format_exc()
+        logger.exception("Main exception:%s" % msg)
+        return "An exception has occurred, check CloudWatch log for more details"
+
+    return {"status": "OK", "message": "Amazon Connect event has been processed"}
+
+
+def compose_connect_body(event_body: dict, context: dict):
+    request_timestamp = context['request_timestamp']
+    chatbot_id = event_body.get("chatbot_config").get("chatbot_id",'admin')
+
+    related_item = event_body["detail"]["relatedItem"]
+    case_id = related_item["caseId"]
+    logger.info(case_id)
+
+    response = connect_client.get_case(
+        caseId=case_id,
+        domainId=connect_domain_id,
+        fields=[
+            {"id": "title"},
+            {"id": "summary"},
+        ],
+    )
+    logger.info(response)
+    case_fields = response["fields"]
+    logger.info(case_fields)
+    title, summary = "", ""
+    for field in case_fields:
+        if field["id"] == "title":
+            title = field["value"]["stringValue"]
+        if field["id"] == "summary":
+            summary = field["value"]["stringValue"]
+    query = title + "\n" + summary
+    logger.info("Query:", query)
+    context_round = event_body.get("context_round", 0)
+
+    # Fix user_id for now
+    user_id = event_body.get("user_id", "default_user_id")
+    # Use case id for session id
+    session_id = case_id
+
+    client_type = event_body.get("client_type", "default_client_type")
+    ddb_history_obj = DynamoDBChatMessageHistory(
+        sessions_table_name=sessions_table_name,
+        messages_table_name=messages_table_name,
+        session_id=session_id,
+        user_id=user_id,
+        client_type=client_type,
+    )
+    chat_history = ddb_history_obj.messages_as_langchain
+
+    agent_flow_body = {}
+    agent_flow_body["query"] = query
+    agent_flow_body["entry_type"] = "common"
+    agent_flow_body["user_profile"] = "default"
+    agent_flow_body["bot_id"] = bot_id
+    agent_flow_body["use_history"] = True
+    agent_flow_body["enable_trace"] = False
+    agent_flow_body["session_id"] = session_id
+    # agent_flow_body["chatbot_config"] = get_bot_info(bot_id)
+    agent_flow_body["chat_history"] = chat_history
+    agent_flow_body["request_timestamp"] = request_timestamp
+    agent_flow_body["user_id"] = user_id
+    agent_flow_body["message_id"] = str(uuid.uuid4())
+    agent_flow_body["context_round"] = context_round
+    agent_flow_body["ddb_history_obj"] = ddb_history_obj
+    agent_flow_body["stream"] = False
+    agent_flow_body["custom_message_id"] = ""
+    agent_flow_body["ws_connection_id"] = ""
+
+    logger.info(agent_flow_body)
+    return agent_flow_body
+
+
 @chatbot_lambda_call_wrapper
 def lambda_handler(event_body:dict, context:dict):
     logger.info(f"raw event_body: {event_body}")
+    entry_type = event_body.get("entry_type", EntryType.COMMON).lower()
+    entry_executor = get_entry(entry_type)
+    if event_body.get("source", "") == "aws.cases":
+        # Connect case event
+        return connect_case_event_handler(event_body, context, entry_executor)
+
     stream = context['stream']
     request_timestamp = context['request_timestamp']
     ws_connection_id = context.get('ws_connection_id')
@@ -71,7 +187,6 @@ def lambda_handler(event_body:dict, context:dict):
         load_ws_client(websocket_url)
 
     client_type = event_body.get("client_type", "default_client_type")
-    entry_type = event_body.get("entry_type", EntryType.COMMON).lower()
     session_id = event_body.get("session_id", None)
     custom_message_id = event_body.get("custom_message_id", "")
     user_id = event_body.get("user_id", "default_user_id")
@@ -106,7 +221,6 @@ def lambda_handler(event_body:dict, context:dict):
     event_body['message_id'] = str(uuid.uuid4())
 
     # logger.info(f"event_body:\n{json.dumps(event_body,ensure_ascii=False,indent=2,cls=JSONEncoder)}")
-    entry_executor = get_entry(entry_type)
     # debuging
     # show debug info directly in local mode
     if is_running_local():
