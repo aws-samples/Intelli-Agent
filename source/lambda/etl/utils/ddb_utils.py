b0e203a843e8f111c482ec74fd16abfccfb30af0
NingLu <lvning19860909@gmail.com>
2024-08-15 09:53:02 +0000

    feat: add index as tool

--- source/lambda/etl/utils/ddb_utils.py
+++ source/lambda/etl/utils/ddb_utils.py
@@ -1,5 +1,5 @@
 from utils.embeddings import get_embedding_info
-from constant import KBType, Status, ModelType, IndexTag
+from constant import KBType, Status, ModelType, IndexTag, IndexType
 from datetime import datetime, timezone
 
 
@@ -38,27 +38,32 @@ def initiate_model(model_table, group_name, model_id, embedding_endpoint, create
     return embedding_info["ModelType"]
 
 
-def initiate_index(index_table, group_name, index_id, model_id, index_type, tag, create_time=None):
+def initiate_index(index_table, group_name, index_id, model_id, index_type, tag, create_time=None, description=""):
     if not create_time:
         create_time = str(datetime.now(timezone.utc))
+
+    db_body = {
+        "groupName": group_name,
+        "indexId": index_id,
+        "indexType": index_type,
+        "kbType": KBType.AOS.value,
+        "modelIds": {
+            "embedding": model_id
+        },
+        "tag": tag,
+        "createTime": create_time,
+        "status": Status.ACTIVE.value
+    }
+    if index_type != IndexType.INTENTION.value:
+        db_body["description"] = description
+
     create_item_if_not_exist(
         index_table,
         {
             "groupName": group_name,
             "indexId": index_id
         },
-        {
-            "groupName": group_name,
-            "indexId": index_id,
-            "indexType": index_type,
-            "kbType": KBType.AOS.value,
-            "modelIds": {
-                "embedding": model_id
-            },
-            "tag": tag,
-            "createTime": create_time,
-            "status": Status.ACTIVE.value
-        }
+        db_body
     )
 
 
