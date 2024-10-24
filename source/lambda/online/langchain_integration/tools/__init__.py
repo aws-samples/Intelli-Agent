52cdcc8377c56180a19681f10c013af3da29d79e
zhouxss <zhouxss@amazon.com>
2024-10-24 03:22:24 +0000

    refactor: unified tool to adapt to langchian's tool

---
+++ source/lambda/online/langchain_integration/tools/__init__.py
@@ -0,0 +1,328 @@
+# from langchain.tools.base import StructuredTool,BaseTool,tool
+# StructuredTool.from_function
+# from langchain_experimental.tools import PythonREPLTool
+# from langchain_core.utils.function_calling import convert_to_openai_function
+# from llama_index.core.tools import FunctionTool
+# from langchain.tools import BaseTool
+# from pydantic import create_model
+
+# from langchain_community.tools import WikipediaQueryRun
+
+
+# builder = StateGraph(State)
+
+
+# # Define nodes: these do the work
+# builder.add_node("assistant", Assistant(part_1_assistant_runnable))
+# builder.add_node("tools", create_tool_node_with_fallback(part_1_tools))
+# # Define edges: these determine how the control flow moves
+# builder.add_edge(START, "assistant")
+# builder.add_conditional_edges(
+#     "assistant",
+#     tools_condition,
+# )
+# builder.add_edge("tools", "assistant")
+
+# # The checkpointer lets the graph persist its state
+# # this is a complete memory for the entire graph.
+# memory = MemorySaver()
+# part_1_graph = builder.compile(checkpointer=memory)
+
+from typing import Optional,Union
+from pydantic import BaseModel
+import platform
+import json 
+import inspect 
+from functools import wraps
+import types 
+
+from datamodel_code_generator import DataModelType, PythonVersion
+from datamodel_code_generator.model import get_data_model_types
+from datamodel_code_generator.parser.jsonschema import JsonSchemaParser
+from langchain.tools.base import StructuredTool,BaseTool
+
+from common_logic.common_utils.constant import SceneType
+from common_logic.common_utils.lambda_invoke_utils import invoke_with_lambda
+
+
+class ToolIdentifier(BaseModel):
+    scene: SceneType
+    name: str
+    
+    @property
+    def tool_id(self):
+        return f"{self.scene}__{self.name}"
+
+
+class ToolManager:
+    tool_map = {}
+
+    @staticmethod
+    def convert_tool_def_to_pydantic(tool_id,tool_def:Union[dict,BaseModel]):
+        if not isinstance(tool_def,dict):
+            return tool_def 
+        # convert tool definition to pydantic model 
+        current_python_version = ".".join(platform.python_version().split(".")[:-1])
+        data_model_types = get_data_model_types(
+            DataModelType.PydanticBaseModel,
+            target_python_version=PythonVersion(current_python_version)
+        )
+        parser = JsonSchemaParser(
+            json.dumps(tool_def,ensure_ascii=False,indent=2),
+            data_model_type=data_model_types.data_model,
+            data_model_root_type=data_model_types.root_model,
+            data_model_field_type=data_model_types.field_model,
+            data_type_manager_type=data_model_types.data_type_manager,
+            dump_resolve_reference_action=data_model_types.dump_resolve_reference_action,
+            use_schema_description=True
+        )
+        result = parser.parse()
+        new_tool_module = types.ModuleType(tool_id)
+        exec(result, new_tool_module.__dict__)
+        return new_tool_module.Model
+
+    
+    @staticmethod
+    def get_tool_identifier(scene=None,name=None,tool_identifier=None):
+        if tool_identifier is None:
+            tool_identifier = ToolIdentifier(scene=scene,name=name)
+        return tool_identifier
+
+
+    @classmethod
+    def register_lc_tool(
+        cls,
+        tool:BaseTool,
+        scene=None,
+        name=None,
+        tool_identifier=None,
+    ):
+        tool_identifier = cls.get_tool_identifier(
+            scene=scene,
+            name=name,
+            tool_identifier=None
+        )
+        assert isinstance(tool,BaseTool),(tool,type(tool))
+        cls.tool_map[tool_identifier.tool_id] = tool 
+    
+
+    @classmethod
+    def register_func_as_tool(
+        cls,
+        func:callable,
+        tool_def:dict,
+        return_direct:False,
+        scene=None,
+        name=None,
+        tool_identifier=None,
+    ):
+        tool_identifier = cls.get_tool_identifier(
+            scene=scene,
+            name=name,
+            tool_identifier=tool_identifier
+        )
+        tool = StructuredTool.from_function(
+            func=func,
+            name=tool_identifier.name,
+            args_schema=ToolManager.convert_tool_def_to_pydantic(
+                tool_id=tool_identifier.tool_id,
+                tool_def=tool_def
+            ),
+            return_direct=return_direct
+        )
+        # register tool 
+        ToolManager.register_lc_tool(
+            tool_identifier=tool_identifier,
+            tool=tool
+        )
+    
+
+    @classmethod
+    def register_aws_lambda_as_tool(
+        cls,
+        lambda_name:str,
+        tool_def:dict,
+        scene=None,
+        name=None,
+        tool_identifier=None,    
+        return_direct=False                     
+        ):
+
+        def _func(**kargs):
+            return invoke_with_lambda(lambda_name,kargs)
+
+        tool_identifier = cls.get_tool_identifier(
+            scene=scene,
+            name=name,
+            tool_identifier=tool_identifier
+        )
+        tool = StructuredTool.from_function(
+            func=_func,
+            name=tool_identifier.name,
+            args_schema=ToolManager.convert_tool_def_to_pydantic(
+                tool_id=tool_identifier.tool_id,
+                tool_def=tool_def
+            ),
+            return_direct=return_direct
+        )
+        ToolManager.register_lc_tool(
+            tool_identifier=tool_identifier,
+            tool=tool
+        )
+
+        
+
+    @classmethod
+    def get_tool(cls, scene, name,**kwargs):
+        # dynamic import 
+        tool_identifier = ToolIdentifier(scene=scene, name=name)
+        tool_id = tool_identifier.tool_id
+        if tool_id not in cls.tool_map:
+            TOOL_MOFULE_LOAD_FN_MAP[tool_id](**kwargs)
+        return cls.tool_map[tool_id]
+
+
+TOOL_MOFULE_LOAD_FN_MAP = {}
+
+
+def lazy_tool_load_decorator(scene:SceneType,name):
+    def decorator(func):
+        tool_identifier = ToolIdentifier(scene=scene, name=name)
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            if "tool_identifier" in inspect.signature(func).parameters:
+                kwargs = {**kwargs,"tool_identifier":tool_identifier}
+            return func(*args, **kwargs)
+        TOOL_MOFULE_LOAD_FN_MAP[tool_identifier.tool_id] = func
+        return wrapper
+    return decorator
+
+
+############################# tool load func ######################
+
+
+@lazy_tool_load_decorator(SceneType.COMMON,"get_weather")
+def _load_common_weather_tool(tool_identifier:ToolIdentifier):
+    from .common_tools import get_weather
+    tool_def = {
+        "description": "Get the current weather for `city_name`",
+        "properties": {
+            "city_name": {
+                "description": "The name of the city to be queried",
+                "type": "string"
+            },
+        },
+        "required": ["city_name"]
+    }
+    ToolManager.register_func_as_tool(
+        tool_identifier.scene,
+        tool_identifier.name,
+        get_weather.get_weather,
+        tool_def,
+        return_direct=False
+    )
+
+
+@lazy_tool_load_decorator(SceneType.COMMON,"give_rhetorical_question")
+def _load_common_rhetorical_tool(tool_identifier:ToolIdentifier):
+    from .common_tools import give_rhetorical_question
+    tool_def = {
+        "description": "If the user's question is not clear and specific, resulting in the inability to call other tools, please call this tool to ask the user a rhetorical question",
+        "properties": {
+            "question": {
+                "description": "The rhetorical question to user",
+                "type": "string"
+            },
+        }
+    } 
+    ToolManager.register_func_as_tool(
+        tool_identifier.scene,
+        tool_identifier.name,
+        give_rhetorical_question.give_rhetorical_question,
+        tool_def,
+        return_direct=True
+    )
+
+
+@lazy_tool_load_decorator(SceneType.COMMON,"give_final_response")
+def _load_common_final_response_tool(tool_identifier:ToolIdentifier):
+    from .common_tools import give_final_response
+    
+    tool_def = {
+        "description": "If none of the other tools need to be called, call the current tool to complete the direct response to the user.",
+        "properties": {
+            "response": {
+                "description": "Response to user",
+                "type": "string"
+            }
+        },
+        "required": ["response"]
+    }
+    ToolManager.register_func_as_tool(
+        tool_identifier.scene,
+        tool_identifier.name,
+        give_final_response.give_final_response,
+        tool_def,
+        return_direct=True
+    )
+
+
+@lazy_tool_load_decorator(SceneType.COMMON,"chat")
+def _load_common_chat_tool(tool_identifier:ToolIdentifier):
+    from .common_tools import chat
+    tool_def = {
+        "description": "casual talk with AI",
+        "properties": {
+            "response": {
+                "description": "response to users",
+                "type": "string"
+                }
+        },
+        "required": ["response"]
+    }
+
+    ToolManager.register_func_as_tool(
+        tool_identifier.scene,
+        tool_identifier.name,
+        chat.chat,
+        tool_def,
+        return_direct=True
+    )
+
+
+@lazy_tool_load_decorator(SceneType.COMMON,"rag_tool")
+def _load_common_rag_tool(tool_identifier:ToolIdentifier):
+    from .common_tools import rag
+    tool_def = {
+        "description": "private knowledge",
+        "properties": {
+            "query": {
+                "description": "query for retrieve",
+                "type": "string"
+                }
+        },
+        "required": ["query"]
+    }
+    ToolManager.register_func_as_tool(
+        tool_identifier.scene,
+        tool_identifier.name,
+        rag.rag,
+        tool_def,
+        return_direct=True
+    )
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
---
+++ source/lambda/online/langchain_integration/tools/common_tools/__init__.py
@@ -0,0 +1,121 @@
+from common_logic.common_utils.constant import SceneType, ToolRuningMode
+from .._tool_base import tool_manager
+from . import (
+    get_weather,
+    give_rhetorical_question,
+    give_final_response,
+    chat,
+    rag
+)
+
+
+SCENE = SceneType.COMMON
+LAMBDA_NAME = "lambda_common_tools"
+
+tool_manager.register_tool({
+    "name": "get_weather",
+    "scene": SCENE,
+    "lambda_name": LAMBDA_NAME,
+    "lambda_module_path": get_weather.lambda_handler,
+    "tool_def": {
+            "name": "get_weather",
+            "description": "Get the current weather for `city_name`",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "city_name": {
+                        "description": "The name of the city to be queried",
+                        "type": "string"
+                    },
+                },
+                "required": ["city_name"]
+            }
+    },
+    "running_mode": ToolRuningMode.LOOP
+})
+
+
+tool_manager.register_tool(
+    {
+        "name": "give_rhetorical_question",
+        "scene": SCENE,
+        "lambda_name": LAMBDA_NAME,
+        "lambda_module_path": give_rhetorical_question.lambda_handler,
+        "tool_def": {
+            "name": "give_rhetorical_question",
+                "description": "If the user's question is not clear and specific, resulting in the inability to call other tools, please call this tool to ask the user a rhetorical question",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "question": {
+                            "description": "The rhetorical question to user",
+                            "type": "string"
+                        },
+                    },
+                    "required": ["question"],
+                },
+        },
+        "running_mode": ToolRuningMode.ONCE
+    }
+)
+
+
+tool_manager.register_tool(
+    {
+        "name": "give_final_response",
+        "scene": SCENE,
+        "lambda_name": LAMBDA_NAME,
+        "lambda_module_path": give_final_response.lambda_handler,
+        "tool_def": {
+                "name": "give_final_response",
+                "description": "If none of the other tools need to be called, call the current tool to complete the direct response to the user.",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "response": {
+                            "description": "Response to user",
+                            "type": "string"
+                        }
+                    },
+                    "required": ["response"]
+                },
+        },
+        "running_mode": ToolRuningMode.ONCE
+    }
+)
+
+
+tool_manager.register_tool({
+    "name": "chat",
+    "scene": SCENE,
+    "lambda_name": LAMBDA_NAME,
+    "lambda_module_path": chat.lambda_handler,
+    "tool_def": {
+        "name": "chat",
+        "description": "casual talk with AI",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "response": {
+                    "description": "response to users",
+                    "type": "string"
+                }},
+            "required": ["response"]
+        },
+    },
+    "running_mode": ToolRuningMode.ONCE
+})
+
+
+tool_manager.register_tool({
+    "name": "rag_tool",
+    "scene": SCENE,
+    "lambda_name": LAMBDA_NAME,
+    "lambda_module_path": rag.lambda_handler,
+    "tool_def": {
+        "name": "rag_tool",
+        "description": "private knowledge",
+        "parameters": {}
+    },
+    "running_mode": ToolRuningMode.ONCE
+})
