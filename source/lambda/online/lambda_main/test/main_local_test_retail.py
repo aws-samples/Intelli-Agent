2d84220d24bea072d664485e2f6de4c861817631
NingLyu <lvning19860909@gmail.com>
2024-11-15 07:49:35 +0000

    chore: update modell id

--- source/lambda/online/lambda_main/test/main_local_test_retail.py
+++ source/lambda/online/lambda_main/test/main_local_test_retail.py
@@ -1,20 +1,21 @@
 from email import message
-from local_test_base import generate_answer,similarity_calculate,auto_evaluation_with_claude
-import time 
-import json 
-import pandas as pd 
-import queue 
+from local_test_base import generate_answer, similarity_calculate, auto_evaluation_with_claude
+import time
+import json
+import pandas as pd
+import queue
 from threading import Thread
-import tqdm 
+import tqdm
 from datetime import datetime
 
 
 CREATE_TIME_FORMAT = '%Y-%m-%d %H:%M:%S.%f'
 CREATE_TIME_FORMAT_2 = '%Y-%m-%d%H:%M:%S.%f'
 
+
 def _test_multi_turns(user_queries, record_goods_id=False):
     session_id = f"anta_test_{time.time()}"
-    
+
     default_llm_config = {
         # 'model_id': 'anthropic.claude-3-haiku-20240307-v1:0',
         # 'model_id': 'anthropic.claude-3-sonnet-20240229-v1:0',
@@ -30,33 +31,34 @@ def _test_multi_turns(user_queries, record_goods_id=False):
         # "endpoint_name":  "Qwen2-72B-Instruct-AWQ-without-yarn-2024-06-29-12-31-04-818",
         # 'model_id': 'mistral.mixtral-8x7b-instruct-v0:1',
         'model_kwargs': {
-            'temperature': 0.01, 
+            'temperature': 0.01,
             'max_tokens': 1000,
-            "repetition_penalty":1.05,
-            "stop_token_ids": [151645,151643],
-            "stop":["<|endoftext|>","<|im_end|>"],
-            "top_k":20,
-            "seed":42,
-            'top_p': 0.8       
-            }
+            "repetition_penalty": 1.05,
+            "stop_token_ids": [151645, 151643],
+            "stop": ["<|endoftext|>", "<|im_end|>"],
+            "top_k": 20,
+            "seed": 42,
+            'top_p': 0.8
+        }
     }
     chatbot_config = {
         "chatbot_mode": "agent",
         "use_history": True,
         "enable_trace": True,
-        "default_llm_config":default_llm_config,
+        "default_llm_config": default_llm_config,
         "intention_config": {
             "query_key": "query"
         }
     }
     if record_goods_id:
-        chatbot_config["history_config"]=['goods_id']
+        chatbot_config["history_config"] = ['goods_id']
     query_answers = []
     for query in user_queries:
-        if isinstance(query,str):
-            query = {"query":query}
-        
-        create_time = query.get('create_time',datetime.now().strftime(CREATE_TIME_FORMAT))
+        if isinstance(query, str):
+            query = {"query": query}
+
+        create_time = query.get(
+            'create_time', datetime.now().strftime(CREATE_TIME_FORMAT))
         try:
             create_time = datetime.strptime(create_time, CREATE_TIME_FORMAT)
         except ValueError:
@@ -65,30 +67,30 @@ def _test_multi_turns(user_queries, record_goods_id=False):
         create_time = create_time.strftime(CREATE_TIME_FORMAT)
 
         r = generate_answer(
-               query=query['query'].replace("/:018",""),
+            query=query['query'].replace("/:018", ""),
             #    create_time=,
-               stream=False,
-                session_id=session_id,
-                chatbot_config={**chatbot_config,"goods_id": query.get("goods_id"),"create_time":create_time},
-                entry_type="retail"
+            stream=False,
+            session_id=session_id,
+            chatbot_config={
+                **chatbot_config, "goods_id": query.get("goods_id"), "create_time": create_time},
+            entry_type="retail"
         )
-        query_answers.append((query['query'],r['message']['content']))
-    
+        query_answers.append((query['query'], r['message']['content']))
+
     print()
     print()
-    for query,ans in query_answers:
+    for query, ans in query_answers:
         print("="*50)
         print(f"human: {query}\nAi: {ans}")
 
 
-
 def test_multi_turns():
     user_queries = [
-        {"query":"我平时穿37，这个鞋合适吗？","goods_id": 756327274174},
-        {"query":"需要运费吗？","goods_id": 743891340644},
-        {"query":"我已经下完单了，什么时候发货？","goods_id": 743891340644},
-        {"query":"身高170，能穿吗？","goods_id": 743891340644},
-        {"query":"45kg","goods_id": 743891340644}]
+        {"query": "我平时穿37，这个鞋合适吗？", "goods_id": 756327274174},
+        {"query": "需要运费吗？", "goods_id": 743891340644},
+        {"query": "我已经下完单了，什么时候发货？", "goods_id": 743891340644},
+        {"query": "身高170，能穿吗？", "goods_id": 743891340644},
+        {"query": "45kg", "goods_id": 743891340644}]
     return _test_multi_turns(user_queries)
 
 
@@ -96,12 +98,12 @@ def test_multi_turns_anta(
         session_id,
         user_queries_path="/efs/projects/aws-samples-llm-bot-branches/aws-samples-llm-bot-dev-online-refactor/source/lambda/online/session_user_queries.json",
         record_goods_id=False
-        ):
+):
     user_queries = json.load(open(user_queries_path))[session_id]
-    return _test_multi_turns(user_queries,record_goods_id=record_goods_id)
-    
+    return _test_multi_turns(user_queries, record_goods_id=record_goods_id)
 
-def batch_test(data_file, count=1000,add_eval_score=True,record_goods_id=False):
+
+def batch_test(data_file, count=1000, add_eval_score=True, record_goods_id=False):
     data = pd.read_csv(data_file).fillna("").to_dict(orient='records')
     session_prefix = f"anta_test_{time.time()}"
     default_llm_config = {
@@ -116,18 +118,18 @@ def batch_test(data_file, count=1000,add_eval_score=True,record_goods_id=False):
         # "endpoint_name":  "Qwen2-7B-Instruct-AWQ-2024-07-10-09-49-39-020",
         # "model_id": "qwen1_5-32B-instruct",
         # "endpoint_name":  "Qwen1-2024-07-10-03-35-21-876",
-        
+
         # "endpoint_name":  "Qwen2-72B-Instruct-AWQ-without-yarn-2024-06-29-12-31-04-818",
         'model_kwargs': {
             'temperature': 0.01, 'max_tokens': 800,
-            "repetition_penalty":1.05,
-            "stop_token_ids": [151645,151643] ,
-            "stop":["<|endoftext|>","<|im_end|>"],
-            "top_k":1,
+            "repetition_penalty": 1.05,
+            "stop_token_ids": [151645, 151643],
+            "stop": ["<|endoftext|>", "<|im_end|>"],
+            "top_k": 1,
             'top_p': 0.8,
-            "seed":42  
-            }
+            "seed": 42
         }
+    }
     chatbot_config = {
         "chatbot_mode": "agent",
         "use_history": True,
@@ -138,22 +140,21 @@ def batch_test(data_file, count=1000,add_eval_score=True,record_goods_id=False):
         }
     }
     if record_goods_id:
-        chatbot_config["history_config"]=['goods_id']
+        chatbot_config["history_config"] = ['goods_id']
 
     if record_goods_id:
-        chatbot_config["history_config"]=['goods_id']
+        chatbot_config["history_config"] = ['goods_id']
 
     save_csv_path = f'anta_test/{session_prefix}_anta_test_{default_llm_config["model_id"]}_{len(data)}.csv'
 
-
-    def _auto_eval_thread_helper(ret_q:queue.Queue):
+    def _auto_eval_thread_helper(ret_q: queue.Queue):
         data_to_save = []
         while True:
             datum = ret_q.get()
             if datum is None:
-                return 
+                return
             ground_truth = datum['ground_truth']
-            print('ground_truth: ',ground_truth,flush=True)
+            print('ground_truth: ', ground_truth, flush=True)
             sim_score = None
             if add_eval_score and datum['ai_msg'] and ground_truth:
                 try:
@@ -161,30 +162,30 @@ def batch_test(data_file, count=1000,add_eval_score=True,record_goods_id=False):
                     sim_score = auto_evaluation_with_claude(
                         ref_answer=str(ground_truth),
                         model_answer=str(datum['ai_msg'])
-                        )
+                    )
                 except Exception as e:
-                    print('auto evaluation error: ',str(e),(str(ground_truth),str(datum['ai_msg'])))
-                    
+                    print('auto evaluation error: ', str(e),
+                          (str(ground_truth), str(datum['ai_msg'])))
 
             data_to_save.append({
                 "session_id": datum['desensitized_cnick'],
                 "goods_id": datum['product_ids'],
                 "create_time": datum['create_time'],
-                "user_msg":datum['user_msg'],
+                "user_msg": datum['user_msg'],
                 "ai_msg": datum['ai_msg'],
                 "ground truth": ground_truth,
                 "sim_score_with_ground_truth": sim_score,
                 "ai_intent": datum['agent_intent_type'],
                 "rewrite_query": datum['query_rewrite'],
-                "trace_infos":str(trace_infos),
+                "trace_infos": str(trace_infos),
                 # "intent": None,
                 # "accuracy": None,
-                "elpase_time":datum['elpase_time'],
+                "elpase_time": datum['elpase_time'],
                 # "ddb_session_id": session_id,
                 # "comments": None,
                 # "owner": None,
                 "model_id": default_llm_config['model_id'],
-                
+
             })
             # session_id, goods_id, create_time, user_msg, ai_msg, ai_intent, intent, accuracy,rewrite_query
             pd.DataFrame(data_to_save).to_csv(
@@ -192,19 +193,19 @@ def batch_test(data_file, count=1000,add_eval_score=True,record_goods_id=False):
                 index=False
             )
 
-    
     ret_q = queue.Queue()
 
-    t = Thread(target=_auto_eval_thread_helper,args=(ret_q,))
+    t = Thread(target=_auto_eval_thread_helper, args=(ret_q,))
     t.start()
 
     # data = data]
     # data_to_save = []
     for datum in tqdm.tqdm(data[:count], total=min(len(data), count)):
-        print("=="*50,flush=True)
+        print("=="*50, flush=True)
         start_time = time.time()
-        print(f'query: {datum["user_msg"]},\ngoods_id: {datum["product_ids"]}',flush=True)
-        
+        print(
+            f'query: {datum["user_msg"]},\ngoods_id: {datum["product_ids"]}', flush=True)
+
         if datum["product_ids"]:
             try:
                 product_ids = int(datum["product_ids"])
@@ -215,56 +216,58 @@ def batch_test(data_file, count=1000,add_eval_score=True,record_goods_id=False):
         else:
             product_ids = None
         session_id = f"{session_prefix}_{datum['desensitized_cnick']}"
-        
 
-        create_time = datum.get('create_time',datetime.now().strftime(CREATE_TIME_FORMAT))
+        create_time = datum.get(
+            'create_time', datetime.now().strftime(CREATE_TIME_FORMAT))
         try:
             create_time = datetime.strptime(create_time, CREATE_TIME_FORMAT)
         except ValueError:
             create_time = datetime.strptime(create_time, CREATE_TIME_FORMAT_2)
         create_time = create_time.strftime(CREATE_TIME_FORMAT)
 
-        chatbot_config.update({"goods_id":product_ids,"create_time":create_time})
+        chatbot_config.update(
+            {"goods_id": product_ids, "create_time": create_time})
         try:
             r = generate_answer(
-                datum['user_msg'].replace("/:018",""),
+                datum['user_msg'].replace("/:018", ""),
                 stream=False,
                 session_id=session_id,
                 chatbot_config=chatbot_config,
                 entry_type="retail"
             )
-            print('r: ',r)
-            trace_infos = r.get("trace_infos",[])
+            print('r: ', r)
+            trace_infos = r.get("trace_infos", [])
             ai_msg = r['message']['content'].strip().rstrip("<|user|>").strip()
         except:
             import traceback
-            print(f"error run:\n {traceback.format_exc()}",flush=True)
+            print(f"error run:\n {traceback.format_exc()}", flush=True)
             ai_msg = None
             r = {}
-            trace_infos=None
+            trace_infos = None
 
-        datum['agent_intent_type'] = r.get('current_agent_intent_type',None)
+        datum['agent_intent_type'] = r.get('current_agent_intent_type', None)
         datum['ai_msg'] = ai_msg
         datum['session_id'] = session_id
-        datum['query_rewrite'] = r.get('query_rewrite',None)
-        datum['intention_fewshot_examples'] = r.get('intention_fewshot_examples',None)
+        datum['query_rewrite'] = r.get('query_rewrite', None)
+        datum['intention_fewshot_examples'] = r.get(
+            'intention_fewshot_examples', None)
         if ai_msg:
             datum['elpase_time'] = time.time()-start_time
         else:
             datum['elpase_time'] = None
-        
-        ground_truth = str(datum.get("ground truth","")).strip()
+
+        ground_truth = str(datum.get("ground truth", "")).strip()
         datum['ground_truth'] = ground_truth
         ret_q.put(datum)
-    
+
     ret_q.put(None)
 
 
 def test_multi_turns_pr(mode="agent"):
     session_id = f"anta_multiturn_test_{time.time()}"
     user_queries = [
-        {"query":"能发顺丰嘛？","goods_id":641874887898, "use_history":True},
-        {"query":"我170能穿吗？","goods_id":641874887898, "use_history":True},
+        {"query": "能发顺丰嘛？", "goods_id": 641874887898, "use_history": True},
+        {"query": "我170能穿吗？", "goods_id": 641874887898, "use_history": True},
     ]
 
     default_llm_config = {
@@ -273,23 +276,25 @@ def test_multi_turns_pr(mode="agent"):
     chatbot_config = {
         "chatbot_mode": mode,
         "enable_trace": True,
-        "default_llm_config":default_llm_config,
+        "default_llm_config": default_llm_config,
         "intention_config": {
             "query_key": "query"
         }
     }
     for query in user_queries:
-        if isinstance(query,str):
-            query = {"query":query}
+        if isinstance(query, str):
+            query = {"query": query}
         chatbot_config['use_history'] = query['use_history']
         r = generate_answer(
-               query=query['query'],
-               stream=False,
-               session_id=session_id,
-               chatbot_config={**chatbot_config,"goods_id": query.get("goods_id")},
-               entry_type="retail"
+            query=query['query'],
+            stream=False,
+            session_id=session_id,
+            chatbot_config={**chatbot_config,
+                            "goods_id": query.get("goods_id")},
+            entry_type="retail"
         )
         print(r['message']['content'])
+
 
 def complete_test():
     print("start test in chat mode")
@@ -307,7 +312,7 @@ if __name__ == "__main__":
     # complete_test()
     test_multi_turns_anta("cn****0094", record_goods_id=True)
     # test_multi_turns()
-    # test_multi_turns_0090() 
+    # test_multi_turns_0090()
     # test_multi_turns_0077()
     # test_multi_turns_pr("agent")
     # batch_test(
@@ -325,7 +330,7 @@ if __name__ == "__main__":
     #     goods_id="675124761798",
     #     query="平常41吗，这款鞋需要多少码"
     # )
-        # query="你家鞋子开胶了，怎么处理？"
+    # query="你家鞋子开胶了，怎么处理？"
     # test(
     #     chatbot_mode='agent',
     #     query="g5.2xlarge ec2的价格是多少"
@@ -344,4 +349,3 @@ if __name__ == "__main__":
     # multi-turn test
 
     # multi_turn_test()
-    
--- source/lambda/online/lambda_main/test/main_websocket_test.py
+++ source/lambda/online/lambda_main/test/main_websocket_test.py
@@ -1,22 +1,23 @@
-import os 
+import json
+import os
 import sys
 import time
 import time
-import dotenv 
+import dotenv
 dotenv.load_dotenv()
 try:
     from websocket import create_connection
 except ModuleNotFoundError:
     os.system(f'{sys.executable} -m pip install websocket-client')
     from websocket import create_connection
-import json 
 
 # ws_url from api gateway
 jwt = os.environ['JWT']
 # ws_url = f"wss://w2druwcuc3.execute-api.us-west-2.amazonaws.com/prod/?idToken={jwt}"
 ws_url = f"wss://rh36ovsp52.execute-api.us-east-1.amazonaws.com/prod?idToken={jwt}"
 
-def get_answer(body,ws):
+
+def get_answer(body, ws):
     ws.send(json.dumps(body))
     start_time = time.time()
     answer = ""
@@ -27,38 +28,39 @@ def get_answer(body,ws):
         if message_type == "START":
             pass
         elif message_type == "CHUNK":
-            print(ret['message']['content'],end="",flush=True)
+            print(ret['message']['content'], end="", flush=True)
         elif message_type == "END":
             break
         elif message_type == "ERROR":
             print(ret['message']['content'])
             break
         elif message_type == "MONITOR":
-            print("monitor info: ",ret['message'])
+            print("monitor info: ", ret['message'])
     return answer
+
 
 def test():
     ws = create_connection(ws_url)
     body = {
         "query": "如何进行个体户备案",
         "entry_type": "common",
-        "session_id":f"test_{time.time()}",
+        "session_id": f"test_{time.time()}",
         "chatbot_config": {
             "chatbot_mode": "agent",
             "use_history": True,
             "use_websearch": False,
-            "default_llm_config":{
-                "model_id": "anthropic.claude-3-sonnet-20240229-v1:0", 
+            "default_llm_config": {
+                "model_id": "anthropic.claude-3-sonnet-20240229-v1:0",
                 "model_kwargs": {"temperature": 0.0, "max_tokens": 4096}
             },
-            "default_index_names":{
-                "qq_match":['bingo_qq']
+            "default_index_names": {
+                "qq_match": ['bingo_qq']
             }
         }
     }
-    r = get_answer(body,ws)
-    ws.close()  
-    return r  
+    r = get_answer(body, ws)
+    ws.close()
+    return r
 
 
 def test_multi_turns():
@@ -68,20 +70,20 @@ def test_multi_turns():
         "query": "今天星期几",
         # "query": "g5.2xlarge ec2的价格是多少",
         "entry_type": "common",
-        "session_id":f"test_{session_id}",
+        "session_id": f"test_{session_id}",
         "chatbot_config": {
             "chatbot_mode": "chat",
             "use_history": True,
             "enable_trace": False,
             "use_websearch": False,
-            "default_llm_config":{
-                "model_id": "anthropic.claude-3-sonnet-20240229-v1:0", 
+            "default_llm_config": {
+                "model_id": "anthropic.claude-3-sonnet-20240229-v1:0",
                 "model_kwargs": {"temperature": 0.0, "max_tokens": 4096}
             }
         }
     }
-    r = get_answer(body,ws)
-    ws.close() 
+    r = get_answer(body, ws)
+    ws.close()
 
     print()
 
@@ -89,58 +91,59 @@ def test_multi_turns():
     body = {
         "query": "今天应该是星期一",
         "entry_type": "common",
-        "session_id":f"test_{session_id}",
+        "session_id": f"test_{session_id}",
         "chatbot_config": {
             "chatbot_mode": "chat",
             "use_history": True,
             "use_websearch": False,
             "enable_trace": False,
-            "default_llm_config":{
-                "model_id": "anthropic.claude-3-sonnet-20240229-v1:0", 
+            "default_llm_config": {
+                "model_id": "anthropic.claude-3-sonnet-20240229-v1:0",
                 "model_kwargs": {"temperature": 0.0, "max_tokens": 4096}
             }
         }
     }
-    r = get_answer(body,ws)
+    r = get_answer(body, ws)
     print()
 
     ws = create_connection(ws_url)
     body = {
         "query": "今天星期几",
         "entry_type": "common",
-        "session_id":f"test_{session_id}",
+        "session_id": f"test_{session_id}",
         "chatbot_config": {
             "chatbot_mode": "rag",
             "use_history": False,
             "use_websearch": False,
             "enable_trace": False,
-            "default_llm_config":{
-                "model_id": "anthropic.claude-3-sonnet-20240229-v1:0", 
+            "default_llm_config": {
+                "model_id": "anthropic.claude-3-sonnet-20240229-v1:0",
                 "model_kwargs": {"temperature": 0.0, "max_tokens": 4096}
             }
         }
     }
-    r = get_answer(body,ws)
-    ws.close() 
+    r = get_answer(body, ws)
+    ws.close()
 
     ws = create_connection(ws_url)
     body = {
         "query": "我们进行了几轮对话",
         "entry_type": "common",
-        "session_id":f"test_{session_id}",
+        "session_id": f"test_{session_id}",
         "chatbot_config": {
             "chatbot_mode": "rag",
             "use_history": True,
             "use_websearch": False,
             "enable_trace": False,
-            "default_llm_config":{
-                "model_id": "anthropic.claude-3-sonnet-20240229-v1:0", 
+            "default_llm_config": {
+                "model_id": "anthropic.claude-3-sonnet-20240229-v1:0",
                 "model_kwargs": {"temperature": 0.0, "max_tokens": 4096}
             }
         }
     }
-    r = get_answer(body,ws)
-    ws.close() 
+    r = get_answer(body, ws)
+    ws.close()
+
 
 if __name__ == "__main__":
     # test_multi_turns()
@@ -148,25 +151,21 @@ if __name__ == "__main__":
     body = {
         "query": "怎么进行个体户备案",
         "entry_type": "common",
-        "session_id":f"test_{time.time()}",
+        "session_id": f"test_{time.time()}",
         "chatbot_config": {
             "chatbot_mode": "rag",
             "use_history": True,
             "use_websearch": False,
             "enable_trace": False,
-            "default_llm_config":{
-                "model_id": "anthropic.claude-3-sonnet-20240229-v1:0", 
+            "default_llm_config": {
+                "model_id": "anthropic.claude-3-sonnet-20240229-v1:0",
                 "model_kwargs": {"temperature": 0.5, "max_tokens": 4096}
             },
-            "default_index_names":{
-                "qq_match":['bingo_qq'],
-                "private_knowledge":['wrong']
+            "default_index_names": {
+                "qq_match": ['bingo_qq'],
+                "private_knowledge": ['wrong']
             },
         }
     }
-    r = get_answer(body,ws)
-    ws.close() 
-
-
-
-    
+    r = get_answer(body, ws)
+    ws.close()
