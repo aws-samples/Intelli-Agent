e2f0d9e5c38ceae54154a2ebcb64421192fd7c65
NingLyu <lvning19860909@gmail.com>
2024-12-18 09:21:18 +0000

    fix: remove history chatbot id after chabotOption is changed

--- source/portal/src/pages/chatbot/ChatBot.tsx
+++ source/portal/src/pages/chatbot/ChatBot.tsx
@@ -39,6 +39,7 @@ import {
   ONLY_RAG_TOOL,
   MODEL_OPTION,
   CURRENT_CHAT_BOT,
+  HISTORY_CHATBOT_ID,
 } from 'src/utils/const';
 import { v4 as uuidv4 } from 'uuid';
 import { MessageDataType, SessionMessage } from 'src/types';
@@ -174,7 +175,7 @@ const ChatBot: React.FC<ChatBotProps> = (props: ChatBotProps) => {
       setChatbotList(getChatbots);
 
       // First try to get chatbotId from history if it exists
-      const historyChatbotId = localStorage.getItem('HISTORY_CHATBOT_ID');
+      const historyChatbotId = localStorage.getItem(HISTORY_CHATBOT_ID);
       const localChatBot = localStorage.getItem(CURRENT_CHAT_BOT);
       
       if (historyChatbotId && getChatbots.some(bot => bot.value === historyChatbotId)) {
@@ -183,11 +184,6 @@ const ChatBot: React.FC<ChatBotProps> = (props: ChatBotProps) => {
           label: historyChatbotId,
           value: historyChatbotId
         });
-
-        localStorage.setItem(CURRENT_CHAT_BOT, JSON.stringify({
-          label: historyChatbotId,
-          value: historyChatbotId
-        }));
       } else if (localChatBot !== null) {
         // Otherwise fall back to local storage
         setChatbotOption(JSON.parse(localChatBot));
@@ -218,7 +214,7 @@ const ChatBot: React.FC<ChatBotProps> = (props: ChatBotProps) => {
       if (sessionMessage && sessionMessage.length > 0) {
         const chatbotId = sessionMessage[0].chatbotId;
         // Store chatbotId for use in getWorkspaceList
-        localStorage.setItem('HISTORY_CHATBOT_ID', chatbotId);
+        localStorage.setItem(HISTORY_CHATBOT_ID, chatbotId);
       }
 
       setMessages(
@@ -834,6 +830,9 @@ const ChatBot: React.FC<ChatBotProps> = (props: ChatBotProps) => {
               selectedOption={chatbotOption}
               onChange={({ detail }) => {
                 setChatbotOption(detail.selectedOption);
+                // Remove history chatbot ID from localStorage when manually changing chatbot
+                // Next time it will only use current_chatbot in localStorage
+                localStorage.removeItem(HISTORY_CHATBOT_ID);
               }}
             />
             <div className="flex-1 pr">
