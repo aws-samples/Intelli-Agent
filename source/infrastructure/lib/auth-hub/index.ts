88954e851bbe8e5016b136d759d2ecba53b594e5
Cui <530051970@qq.com>
2025-03-05 15:57:13 +0800

    update auth-hub authorizer

--- source/infrastructure/lib/auth-hub/index.ts
+++ source/infrastructure/lib/auth-hub/index.ts
@@ -11,7 +11,7 @@
  *  and limitations under the License.
  */
 
-import { LambdaIntegration } from 'aws-cdk-lib/aws-apigateway';
+import { IdentitySource, LambdaIntegration, RequestAuthorizer } from 'aws-cdk-lib/aws-apigateway';
 import { Function, Code, Runtime, LayerVersion, Alias } from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
 import * as apigw from "aws-cdk-lib/aws-apigateway";
@@ -69,16 +69,8 @@ export interface AuthHubProps {
           handler: 'auth_api.handler',
           memorySize: 4096,
           timeout: Duration.seconds(10),
-          // environment: {
-          //   cognito_client_id: userPoolClient?.userPoolClientId,
-          //   user_pool_id: userPool?.userPoolId,
-          //   cognito_domain: userPoolDomain.domainName,
-          //   region: props.region
-          // }, 
           layers: [authLayer]
         });
-
-        // authFunction.node.addDependency(userPoolClient)
 
         const version = authFunction.currentVersion;
         new Alias(this, 'AuthFunctionAlias', {
@@ -86,67 +78,28 @@ export interface AuthHubProps {
             version,
             provisionedConcurrentExecutions: 2,
         });
-    
-        // this.apigw = new RestApi(this, `${solutionName}AuthAPI`, {
-        //   restApiName: `${solutionName}AuthAPI Service`,
-        //   endpointConfiguration: {
-        //     types: [EndpointType.REGIONAL],
-        //   },
-        //   defaultCorsPreflightOptions: {
-        //     allowHeaders: [
-        //       "Content-Type",
-        //       "X-Amz-Date",
-        //       "Authorization",
-        //       "OIDC-Issuer",
-        //       "X-Api-Key",
-        //       "X-Amz-Security-Token",
-        //     ],
-        //     allowMethods: Cors.ALL_METHODS,
-        //     allowCredentials: true,
-        //     allowOrigins: Cors.ALL_ORIGINS,
-        //   },
-        //   deployOptions:{
-        //     stageName: stage,
-        //     loggingLevel: MethodLoggingLevel.INFO,
-        //     dataTraceEnabled: true,
-        //     metricsEnabled: true
-        //   }
-        // });
+
+        const authAuthorizerFunction = new Function(this, `${solutionName}AuthAuthorizerFunction`, {
+          runtime: Runtime.PYTHON_3_12,
+          code: Code.fromAsset('../../../lambda/auth-hub'),
+          handler: 'authorizer.handler'
+        });
+
+        const authAuthorizer = new RequestAuthorizer(this, `${solutionName}AuthAuthorizer`, {
+          handler: authAuthorizerFunction,
+          identitySources: [
+            IdentitySource.header('x-authing-lang'),
+          ],
+        });
         const authIntegration = new LambdaIntegration(authFunction)
         const authResource = apiGateway.root.addResource('auth')
         const loginResource = authResource.addResource('login')
-        loginResource.addMethod('POST', authIntegration);
+        loginResource.addMethod('POST', authIntegration, {authorizer: authAuthorizer});
         const tokenResource = authResource.addResource('token')
         const verifyResource = tokenResource.addResource('verify');
         verifyResource.addMethod('GET', authIntegration);
         const refreshResource = tokenResource.addResource('refresh');
         refreshResource.addMethod('POST', authIntegration);
-
-        // const configFile = 'auth.json';
-        // new AwsCustomResource(this, 'WebConfig', {
-        //   logRetention: RetentionDays.ONE_DAY,
-        //   onUpdate: {
-        //     action: 'putObject',
-        //     parameters: {
-        //       Body: JSON.stringify({
-        //         api_url: this.apigw.url,
-        //         app_url: props.distribution.distributionDomainName,
-        //       }),
-        //       Bucket: props.portalBucket.bucketName,
-        //       CacheControl: 'max-age=0, no-cache, no-store, must-revalidate',
-        //       ContentType: 'application/json',
-        //       Key: configFile,
-        //     },
-        //     service: 'S3',
-        //     physicalResourceId: PhysicalResourceId.of(`config-${randomTag}`),
-        //   },
-        //   policy: AwsCustomResourcePolicy.fromStatements([
-        //     new PolicyStatement({
-        //       actions: ['s3:PutObject'],
-        //       resources: [props.portalBucket.arnForObjects(configFile)]
-        //     })
-        //   ])
-        // });
       }
   }
   
