b5796467824bbb55ff64c79ad1c8481b64bd7546
NingLu <lvning19860909@gmail.com>
2025-03-06 16:16:11 +0800

    chore: merge

--- source/portal/src/comps/LoginCallback.tsx
+++ source/portal/src/comps/LoginCallback.tsx
@@ -1,12 +1,43 @@
-import React, { useEffect } from 'react';
 import { Spinner } from '@cloudscape-design/components';
+import React, { useEffect } from 'react';
+import { useAuth } from 'react-oidc-context';
+import useAxiosRequest from 'src/hooks/useAxiosRequest';
+import { LAST_VISIT_URL } from 'src/utils/const';
 
 const LoginCallback: React.FC = () => {
-  useEffect(() => {
-    // Simply redirect to home page
-    window.location.href = '/';
-  }, []);
+  const fetchData = useAxiosRequest();
+  const auth = useAuth();
+  const gotoBasePage = () => {
+    const lastVisitUrl = localStorage.getItem(LAST_VISIT_URL) ?? '/';
+    localStorage.removeItem(LAST_VISIT_URL);
+    window.location.href = `${lastVisitUrl}`;
+  };
 
+  const createDefaultChatBotIfNotExist = async () => {
+    const groupName: string[] = auth?.user?.profile?.['cognito:groups'] as any;
+    const existed = await fetchData({
+      url: 'chatbot-management/default-chatbot',
+      method: 'get'
+    })
+    if(existed){
+      gotoBasePage();
+      return;
+    }
+    const data = await fetchData({
+      url: 'chatbot-management/chatbots',
+      method: 'post',
+      data: {
+        groupName: groupName?.[0] ?? 'Admin',
+      },
+    });
+    if (data.chatbotId) {
+      gotoBasePage();
+    }
+  };
+
+  useEffect(() => {
+    createDefaultChatBotIfNotExist();
+  }, []);
   return (
     <div className="page-loading">
       <Spinner />
