82496ebcba3f5a9c6c07d3e3f8b7ba109fc51006
NingLu <lvning19860909@gmail.com>
2025-03-06 15:21:51 +0800

    chore: merge

--- source/lambda/authorizer/custom_authorizer.py
+++ source/lambda/authorizer/custom_authorizer.py
@@ -7,18 +7,18 @@ from urllib.request import urlopen
 import jwt
 
 # Replace with your Cognito User Pool info
-USER_POOL_ID = os.getenv("USER_POOL_ID", "")
-REGION = os.getenv("REGION", "")
-APP_CLIENT_ID = os.getenv("APP_CLIENT_ID", "")
+USER_POOL_ID = os.environ["USER_POOL_ID"]
+REGION = os.environ["REGION"]
+APP_CLIENT_ID = os.environ["APP_CLIENT_ID"]
 verify_exp = os.getenv("mode") != "dev"
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-# issuer = f"https://cognito-idp.{REGION}.amazonaws.com/{USER_POOL_ID}"
-# keys_url = f"https://cognito-idp.{REGION}.amazonaws.com/{USER_POOL_ID}/.well-known/jwks.json"
-# response = urlopen(keys_url)
-# keys = json.loads(response.read())["keys"]
+issuer = f"https://cognito-idp.{REGION}.amazonaws.com/{USER_POOL_ID}"
+keys_url = f"https://cognito-idp.{REGION}.amazonaws.com/{USER_POOL_ID}/.well-known/jwks.json"
+response = urlopen(keys_url)
+keys = json.loads(response.read())["keys"]
 
 
 def generatePolicy(principalId, effect, resource, claims):
@@ -55,62 +55,58 @@ def generateDeny(principalId, resource, claims):
 def lambda_handler(event, context):
     logger.info(event)
     try:
-        # if event.get("httpMethod"):
-        #     # REST API
-        #     if event["headers"].get("authorization"):
-        #         # Browser will change the Authorization header to lowercase
-        #         token = event["headers"]["authorization"].replace("Bearer", "").strip()
-        #     else:
-        #         # Postman
-        #         token = event["headers"]["Authorization"].replace("Bearer", "").strip()
-        # else:
-        #     # WebSocket API
-        #     token = event["queryStringParameters"]["idToken"]
+        if event.get("httpMethod"):
+            # REST API
+            if event["headers"].get("authorization"):
+                # Browser will change the Authorization header to lowercase
+                token = (
+                    event["headers"]["authorization"]
+                    .replace("Bearer", "")
+                    .strip()
+                )
+            else:
+                # Postman
+                token = (
+                    event["headers"]["Authorization"]
+                    .replace("Bearer", "")
+                    .strip()
+                )
+        else:
+            # WebSocket API
+            token = event["queryStringParameters"]["idToken"]
 
-        # headers = jwt.get_unverified_header(token)
-        # kid = headers["kid"]
+        headers = jwt.get_unverified_header(token)
+        kid = headers["kid"]
 
-        # # Search for the kid in the downloaded public keys
-        # key_index = -1
-        # for i in range(len(keys)):
-        #     if kid == keys[i]["kid"]:
-        #         key_index = i
-        #         break
-        # if key_index == -1:
-        #     logger.error("Public key not found in jwks.json")
-        #     raise Exception(
-        #         "Custom Authorizer Error: Public key not found in jwks.json"
-        #     )
+        # Search for the kid in the downloaded public keys
+        key_index = -1
+        for i in range(len(keys)):
+            if kid == keys[i]["kid"]:
+                key_index = i
+                break
+        if key_index == -1:
+            logger.error("Public key not found in jwks.json")
+            raise Exception(
+                "Custom Authorizer Error: Public key not found in jwks.json"
+            )
 
-        # # Construct the public key
-        # public_key = jwt.algorithms.RSAAlgorithm.from_jwk(json.dumps(keys[key_index]))
+        # Construct the public key
+        public_key = jwt.algorithms.RSAAlgorithm.from_jwk(
+            json.dumps(keys[key_index])
+        )
 
-        # # Verify the signature of the JWT token
-        # claims = jwt.decode(
-        #     token, public_key, algorithms=["RS256"], audience=APP_CLIENT_ID,
-        #     issuer=issuer, options={"verify_exp": verify_exp}
-        # )
-        # # reformat claims to align with cognito output
-        # claims["cognito:groups"] = ",".join(claims["cognito:groups"])
-        # logger.info(claims)
-
-        claims = {
-            "at_hash": "test",
-            "sub": "test",
-            "cognito:groups": "Admin",
-            "email_verified": True,
-            "iss": "https://cognito-idp.us-west-2.amazonaws.com/us-west-2_xxxxx",
-            "cognito:username": "xxxxx",
-            "origin_jti": "xxxxx",
-            "aud": "xxxxx",
-            "event_id": "xxxxx",
-            "token_use": "id",
-            "auth_time": 1739935485,
-            "exp": 1739939085,
-            "iat": 1739935485,
-            "jti": "xxxxx",
-            "email": "xxxxx",
-        }
+        # Verify the signature of the JWT token
+        claims = jwt.decode(
+            token,
+            public_key,
+            algorithms=["RS256"],
+            audience=APP_CLIENT_ID,
+            issuer=issuer,
+            options={"verify_exp": verify_exp},
+        )
+        # reformat claims to align with cognito output
+        claims["cognito:groups"] = ",".join(claims["cognito:groups"])
+        logger.info(claims)
 
         response = generateAllow("me", "*", claims)
         logger.info("Authorized")
