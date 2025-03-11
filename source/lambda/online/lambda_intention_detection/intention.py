3ebfd439bc0736d9d256fb0ae0fd6d642b00d991
zhouxss <zhouxss@amazon.com>
2025-03-11 01:37:09 +0000

    modify online intention

--- source/lambda/online/lambda_intention_detection/intention.py
+++ source/lambda/online/lambda_intention_detection/intention.py
@@ -1,7 +1,9 @@
 import json
 import pathlib
 import os
-
+import asyncio
+from typing import List
+from langchain_core.documents import Document
 from shared.utils.logger_utils import get_logger
 from shared.utils.lambda_invoke_utils import chatbot_lambda_call_wrapper
 from shared.langchain_integration.retrievers import OpensearchHybridQueryQuestionRetriever
@@ -36,29 +38,48 @@ def get_intention_results(query: str, intention_config: dict, intent_threshold: 
     #     handler_name="lambda_handler",
     #     event_body=event_body
     # )
-    OpensearchHybridQueryQuestionRetriever.from_config(
-        intention_config["retrievers"][0]["target_model"]
+    intention_retriever = OpensearchHybridQueryQuestionRetriever.from_config(
+        **event_body
     )
-    res = retrieve_fn(event_body)
+    intention_retrievered:List[Document] = asyncio.run(intention_retriever.ainvoke(event_body['query']))
+    # res = retrieve_fn(event_body)
 
-    if not res["result"]["docs"]:
+    if not intention_retrievered:
         # Return to guide the user to add intentions
         return [], False
     else:
         intent_fewshot_examples = []
-        for doc in res["result"]["docs"]:
+        for doc in intention_retrievered:
             # if "titan-embed-text-v1" in intention_config["retrievers"][0]["target_model"]:
             #     # Titan v1 threshold is 0.001, Titan v2 threshold is 0.4
             #     threshold_score = 0.001
-            if doc["score"] > intent_threshold:
+            if doc.metadata["score"] > intent_threshold:
                 doc_item = {
-                    "query": doc["page_content"],
-                    "score": doc["score"],
-                    "name": doc["answer"],
-                    "intent": doc["answer"],
-                    "kwargs": doc.get("kwargs", {}),
+                    "query": doc.page_content,
+                    "score": doc.metadata["score"],
+                    "name": doc.metadata["answer"],
+                    "intent": doc.metadata["answer"],
+                    "kwargs": doc.metadata("kwargs", {}),
                 }
                 intent_fewshot_examples.append(doc_item)
+    # if not res["result"]["docs"]:
+    #     # Return to guide the user to add intentions
+    #     return [], False
+    # else:
+    #     intent_fewshot_examples = []
+    #     for doc in res["result"]["docs"]:
+    #         # if "titan-embed-text-v1" in intention_config["retrievers"][0]["target_model"]:
+    #         #     # Titan v1 threshold is 0.001, Titan v2 threshold is 0.4
+    #         #     threshold_score = 0.001
+    #         if doc["score"] > intent_threshold:
+    #             doc_item = {
+    #                 "query": doc["page_content"],
+    #                 "score": doc["score"],
+    #                 "name": doc["answer"],
+    #                 "intent": doc["answer"],
+    #                 "kwargs": doc.get("kwargs", {}),
+    #             }
+    #             intent_fewshot_examples.append(doc_item)
 
     return intent_fewshot_examples, True
 
