24a033990bc032e03acd42068f3163ddccd1c1b4
Cui <530051970@qq.com>
2025-03-25 17:49:38 +0800

    fix bugs in bedrock api calling

--- source/lambda/shared/langchain_integration/models/chat_models/brconnector_bedrock_models.py
+++ source/lambda/shared/langchain_integration/models/chat_models/brconnector_bedrock_models.py
@@ -1,5 +1,9 @@
+import json
+from pydantic import Field
 import os
+from typing import Any
 import boto3
+from shared.utils.secret_utils import get_secret_value
 
 from shared.constant import (
     MessageType,
@@ -7,13 +11,22 @@ from shared.constant import (
     ModelProvider
 )
 from shared.utils.logger_utils import get_logger
-from . import ChatModelBase
+from langchain_openai import ChatOpenAI as _ChatOpenAI
+from . import BedrockConverseReasonModelResult, BedrockConverseReasonModelStreamResult, ChatModelBase
 from ..model_config import BEDROCK_MODEL_CONFIGS
-from langchain_openai import ChatOpenAI
-from shared.constant import LLMModelType
 
 
 logger = get_logger("brconnector_bedrock_model")
+
+class ChatOpenAI(_ChatOpenAI):
+    enable_any_tool_choice: bool = False
+    any_tool_choice_value: str = 'any'
+    enable_prefill: bool = True
+    is_reasoning_model: bool = False
+    reason_model_result_cls: Any = BedrockConverseReasonModelResult
+    reason_model_result_cls_init_kwargs:dict = Field(default_factory=dict)
+    reason_model_stream_result_cls: Any = BedrockConverseReasonModelStreamResult
+    reason_model_stream_result_cls_init_kwargs:dict = Field(default_factory=dict)
 
 
 BRCONNECTOR_MODEL_MAP = {
@@ -49,8 +62,8 @@ class BrconnectorBedrockBaseModel(ChatModelBase):
         model_kwargs = {**cls.default_model_kwargs, **model_kwargs}
         base_url = kwargs.get("base_url", None) or os.environ.get(
             "BRCONNECTOR_API_URL", None)
-        api_key = kwargs.get('br_api_key', None) or os.environ.get(
-            "BR_API_KEY", None)
+        api_key_arn = kwargs.get('api_key_arn', None) or os.environ.get(
+            "BR_API_KEY_ARN", None)
 
         assert base_url, ("base_url is required", kwargs)
 
@@ -58,9 +71,9 @@ class BrconnectorBedrockBaseModel(ChatModelBase):
             enable_any_tool_choice=cls.enable_any_tool_choice,
             enable_prefill=cls.enable_prefill,
             base_url=base_url,
-            api_key=api_key,
+            api_key=json.loads(get_secret_value(api_key_arn)),
             model=BRCONNECTOR_MODEL_MAP[cls.model_id],
-            is_reasoning_model = cls.is_reasoning_model
+            is_reasoning_model = cls.is_reasoning_model,
             **model_kwargs
         )
 
